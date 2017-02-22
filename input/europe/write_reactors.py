6f9431b4b151f2f989d2dfb5f46061b4ae516120
Jin Whan Bae <jbae11@illinois.edu>
2017-02-22 16:26:03 -0600

    edit write_reactors.py to create region input file

--- input/europe/write_reactors.py
+++ input/europe/write_reactors.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # to run:
 if len(sys.argv) < 3:
-    print('Usage: python write_reactors.py [csv] [template] [output]')
+    print('Usage: python write_reactors.py [csv] [template1] [template2] [output1] [output2]')
 
 # loads reactor info
 reactor_lists = np.genfromtxt(sys.argv[1],
@@ -18,6 +18,11 @@ with open(sys.argv[2], 'r') as fp:
     input_template = fp.read()
     template = jinja2.Template(input_template)
 
+#takes third argument file as region template
+with open(sys.argv[3],'r') as ft:
+	input_template2 = ft.read()
+	template2 = jinja2.Template(input_template2)
+
 # for every row, replace the following items with values in the csv file.
 for reactor in reactor_lists:
     reactor_body = \
@@ -25,5 +30,13 @@ for reactor in reactor_lists:
     	            reactor_name=reactor['reactor_name'].decode('utf-8'),
                     n_assem_core=reactor['n_assem_core'],
                     n_assem_batch=reactor['n_assem_batch'])
-    with open(sys.argv[3], 'a') as output:
+    with open(sys.argv[4], 'a') as output:
         output.write(reactor_body)
+
+# ((region template)) render
+for reactor in reactor_lists:
+	region_body= \
+	template2.render(country=reactor['country'].decode('utf-8'),
+		             reactor_name=reactor['reactor_name'].decode('utf-8'))
+	with open(sys.argv[5],'a') as output:
+		output.write(region_body)
