76b8057388f3b4f0d591bc334f66f0b57a759b58
Jin Whan Bae <jbae11@illinois.edu>
2017-03-14 11:58:03 -0500

    PEP8

--- input/write_reactors.py
+++ input/write_reactors.py
@@ -5,8 +5,9 @@ import os
 
 # tells command format if input is invalid
 if len(sys.argv) < 6:
-    print('Usage: python write_reactors.py [csv] [reactor_template] [deployinst_template]\
-        [input_template] [reactor_output] [region_output]')
+    print('Usage: python write_reactors.py [csv]\
+          [reactor_template] [deployinst_template]\
+          [input_template] [reactor_output] [region_output]')
 
 
 def delete_file(file):
@@ -42,24 +43,32 @@ def read_csv(csv_file):
 
     """
 
-    reactor_lists = np.genfromtxt(csv_file, skip_header = 1,
+    reactor_lists = np.genfromtxt(csv_file,
+                                  skip_header=1,
                                   delimiter=',',
-                                  dtype=('S128', 'S128', 'S128', 'int', 
-                                         'int', 'int', 'S128',
-                                         'S128', 'int', 'int', 'int',
-                                         'int', 'int', 'int',
-                                         'int', 'int', 'float'),
-                                  names=('country', 'reactor_name', 'type', 'capacity',
-                                         'n_assem_core', 'n_assem_batch','status',
-                                         'operator', 'const_date', 'cons_year', 'first_crit',
-                                         'entry_time', 'lifetime', 'first_grid',
-                                         'commercial', 'shutdown_date', 'ucf'))
+                                  dtype=('S128', 'S128',
+                                         'S128', 'int',
+                                         'int', 'int',
+                                         'S128', 'S128', 'int',
+                                         'int', 'int',
+                                         'int', 'int',
+                                         'int', 'int',
+                                         'int', 'float'),
+                                  names=('country', 'reactor_name',
+                                         'type', 'capacity',
+                                         'n_assem_core', 'n_assem_batch',
+                                         'status', 'operator', 'const_date',
+                                         'cons_year', 'first_crit',
+                                         'entry_time', 'lifetime',
+                                         'first_grid', 'commercial',
+                                         'shutdown_date', 'ucf'))
 
     return reactor_lists
 
+
 def get_ymd(yyyymmdd):
     """This function extracts year and month value from yyyymmdd format
-        
+
         The month value is rounded up if the day is above 16
 
     Prameters
@@ -81,6 +90,7 @@ def get_ymd(yyyymmdd):
     if day > 16:
         month += 1
     return (year, month)
+
 
 def get_lifetime(start_date, end_date):
     """This function gets the lifetime for a prototype given the
@@ -105,7 +115,7 @@ def get_lifetime(start_date, end_date):
         start_year, start_month = get_ymd(start_date)
         dyear = end_year - start_year
         dmonth = end_month - start_month
-        if dmonth<0:
+        if dmonth < 0:
             dyear -= 1
             start_month += 12
         dmonth = end_month - start_month
@@ -113,6 +123,7 @@ def get_lifetime(start_date, end_date):
         return (12 * dyear + dmonth)
     else:
         return 720
+
 
 def get_entrytime(init_date, start_date):
     """This function converts the date format and saves it in variables.
@@ -125,20 +136,20 @@ def get_entrytime(init_date, start_date):
         start date of simulation
     start_date: int
         start date of reactor - first criticality.
-    
+
     Returns
     -------
     entry_time: int
         timestep of the prototype to enter
-    
+
     """
 
     init_year, init_month = get_ymd(init_date)
     start_year, start_month = get_ymd(start_date)
-    
+
     dyear = start_year-init_year
     dmonth = start_month - init_month
-    if dmonth<0:
+    if dmonth < 0:
         dyear -= 1
         start_month += 12
     dmonth = start_month - init_month
@@ -150,9 +161,9 @@ def get_entrytime(init_date, start_date):
         if lifetime < 0:
             lifetime = 0
         entry_time = 0
-    
+
     return entry_time
-    
+
 
 def read_template(template):
     """ Returns a jinja template
@@ -234,11 +245,12 @@ def input_render(init_date, reactor_file, region_file, template, output_file):
     startmonth = (init_date // 100) % 100
     startyear = init_date // 10000
 
-    temp = template.render(startmonth = startmonth, startyear = startyear,
+    temp = template.render(startmonth=startmonth, startyear=startyear,
                            reactor_input=reactor, region_input=region)
 
     with open(output_file, 'a') as output:
         output.write(temp)
+
 
 def region_render(array, template, full_template, output_file):
     """Takes the array and template and writes a region file
@@ -261,7 +273,7 @@ def region_render(array, template, full_template, output_file):
     """
 
     country_list = []
-    empty_country =[]
+    empty_country = []
 
     valhead = '<val>'
     valtail = '</val>'
@@ -278,16 +290,20 @@ def region_render(array, template, full_template, output_file):
         lifetime = ''
 
         for data in array:
-            if (data['country'].decode('utf-8') == country and data['entry_time'] != 0):
-                prototype += valhead + data['reactor_name'].decode('utf-8') +valtail + '\n'
-                entry_time += valhead + str(data['entry_time']) +valtail + '\n'
-                number += valhead + '1' + valtail +'\n'
-                lifetime += valhead + str(data['lifetime']) +valtail + '\n'
-     
-        render_temp = template.render(prototype = prototype,
-                                      start_time = entry_time,
-                                      number = number,
-                                      lifetime = lifetime)
+            if (data['country'].decode('utf-8') == country and
+                data['entry_time'] != 0):
+                prototype += valhead
+                             + data['reactor_name'].decode('utf-8')
+                             + valtail + '\n'
+                entry_time += valhead
+                              + str(data['entry_time']) + valtail + '\n'
+                number += valhead + '1' + valtail + '\n'
+                lifetime += valhead + str(data['lifetime']) + valtail + '\n'
+
+        render_temp = template.render(prototype=prototype,
+                                      start_time=entry_time,
+                                      number=number,
+                                      lifetime=lifetime)
         if len(render_temp) > 110:
             with open(country, 'a') as output:
                 output.write(render_temp)
@@ -296,16 +312,16 @@ def region_render(array, template, full_template, output_file):
 
     for country in empty_country:
         country_set.remove(country)
-       
 
-    # create array of countries and create files for each country.
     for country in country_set:
         # jinja render region template for different countries
         with open(country, 'r') as ab:
             country_input = ab.read()
-            country_body = full_template.render(country=country,
-                                                country_gov=country + '_government',
-                                                deployinst = country_input)
+            country_body = full_template.render(
+                                                country=country,
+                                                country_gov=country
+                                                            + '_government',
+                                                deployinst=country_input)
 
         # write rendered template as 'country'_region
         with open(country + '_region', 'a') as output:
@@ -349,9 +365,8 @@ def main(csv_file, reactor_template, deployinst_template,
     delete_file(reactor_output)
     delete_file(region_output)
 
-    #initialize initial values form (yyyymmdd)
+    # initialize initial values form (yyyymmdd)
     init_date = 19400101
-
 
     # read csv and templates
     dataset = read_csv(csv_file)
@@ -364,12 +379,15 @@ def main(csv_file, reactor_template, deployinst_template,
         entry_time = get_entrytime(init_date, data['first_crit'])
         lifetime = get_lifetime(data['first_crit'], data['shutdown_date'])
         data['entry_time'] = entry_time
-        data['lifetime'] = lifetime       
+        data['lifetime'] = lifetime
     # renders reactor / region / input file. Confesses imperfection.
     reactor_render(dataset, reactor_template, reactor_output)
-    region_render(dataset, deployinst_template, region_output_template, region_output)
-    input_render(init_date, reactor_output, region_output, input_template, 'complete_input.xml')
-    print('\n Insert sink and source into the regions - updates to come! :) \n ')
+    region_render(dataset, deployinst_template,
+                  region_output_template, region_output)
+    input_render(init_date, reactor_output, region_output,
+                 input_template, 'complete_input.xml')
+    print('\n Insert sink and source into the regions \
+          - updates to come! :) \n ')
 
 
 if __name__ == "__main__":
