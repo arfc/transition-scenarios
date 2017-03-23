22af70d30142ac866cfde6bcc8a697ae5f90cbf3
Jin Whan Bae <jbae11@illinois.edu>
2017-03-22 21:55:45 -0500

    analysis can take many qualids

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -26,15 +26,19 @@ def snf(filename, cursor):
     -------
     Returns total snf and isotope mass.
     """
+
     cur = cursor
 
     sink_id = get_sink_agent_ids(cur)
+
+    # get resources that ended up in sink.
     resources = cur.execute(exec_string(sink_id,
                                         'transactions.receiverId',
                                         '*')).fetchall()
+    # get array of sum(quantity) and qualid for snf
     snf_inventory = cur.execute(exec_string(sink_id,
                                             'transactions.receiverId',
-                                            'sum(quantity)')).fetchall()[0][0]
+                                            'sum(quantity), qualid') + ' group by qualid').fetchall()
     waste_id = get_waste_id(resources)
     inven = isotope_calc(waste_id, snf_inventory, cur)
     return inven
@@ -88,6 +92,121 @@ def get_waste_id(resource_array):
         wasteid.append(res[7])
 
     return set(wasteid)
+
+
+def exec_string(array, search, whatwant):
+    """ Generates sqlite query command to select things an
+        inner join between resources and transactions.
+
+    Parmaters
+    ---------
+    array: array
+        array of criteria that generates command
+    search: str
+        where [search]
+        criteria for your search
+    whatwant: str
+        select [whatwant]
+        column (set of values) you want out.
+
+    Returns
+    -------
+    exec_str: str
+        sqlite query command.
+    """
+
+    exec_str = ('select ' + whatwant + ' from resources inner join transactions\
+                on transactions.resourceid = resources.resourceid where '
+                + str(search) + ' = ' + str(array[0]))
+
+    for ar in array[1:]:
+        exec_str += ' or ' + str(ar)
+
+    return exec_str
+
+
+def get_sum(array, column_index):
+    """ Returns sum of a column in an array
+
+    Paramters:
+    ---------
+    array: array
+        array that contains a column with numbers
+    column_index: int
+        index for the column to be summed
+    """
+    sum = 0
+    for ar in array:
+        sum += ar[column_index]
+
+    return sum
+
+
+
+def isotope_calc(wasteid_array, snf_inventory, cursor):
+    """ Calculates isotope mass using mass fraction in compositions table.
+
+        Fetches all from compositions table.
+        Compositions table has the following format:
+            SimId / QualId / NucId / MassFrac
+        Then sees if the qualid matches, and if it multiplies
+        the mass fraction by the snf_inventory.
+
+    Prameters
+    ---------
+    wasteid_array: array
+        array of qualid of wastes
+    snf_inventory: float
+        total mass of snf
+    cursor: cursor
+        cursor for sqlite3
+
+    Returns
+    -------
+    nuclide_inven: array
+        inventory of individual nuclides.
+    """
+
+    # Get compositions of different commodities
+    # SimId / QualId / NucId / MassFrac
+    cur = cursor
+    comp = cur.execute('select * from compositions').fetchall()
+    total_snf_mass = get_sum(snf_inventory, 0)
+
+    nuclide_inven = 'total snf inventory = ' + str(total_snf_mass) + 'kg \n'
+    nuclides = []
+    mass_of_nuclides = []
+    # if the 'qualid's match,
+    # the nuclide quantity and calculated and displayed.
+    for isotope in comp:
+        for num in snf_inventory:
+            # num[1] = snf inventory qualid 
+            # isotope[1] = compositions qualid 
+            if num[1] == isotope[1]:
+                # num[0] = total mass of one composition
+                # isotope[3] = mass fraction
+                nuclide_quantity = num[0] * isotope[3]
+                #isotope[2] = nucid
+                nuclide_name = isotope[2]
+                nuclides.append(nuclide_name)
+                mass_of_nuclides.append(nuclide_quantity)
+
+    nuclide_set = set(nuclides)
+    mass_dict = collections.OrderedDict({})
+
+    for nuclide in nuclide_set:
+        temp_nuclide_sum = 0 
+        for i in range(len(nuclides)):
+            if nuclides[i] == nuclide:
+                temp_nuclide_sum += mass_of_nuclides[i]
+        mass_dict[nuclide] = temp_nuclide_sum
+
+
+    for nuclide in mass_dict:
+        nuclide_name = str(nucname.name(nuclide))
+        nuclide_inven += nuclide_name + ' = ' + str(mass_dict[nuclide]) + 'kg \n'
+
+    return nuclide_inven
 
 
 def capacity_calc(governments, timestep, entry, exit):
@@ -247,78 +366,6 @@ def plot_power(filename, cursor):
                       'Time', 'num_reactors',
                       'Number of Reactors vs Time', 'number_plot.png')
 
-
-def exec_string(array, search, whatwant):
-    """ Generates sqlite query command to select things an
-        inner join between resources and transactions.
-
-    Parmaters
-    ---------
-    array: array
-        array of criteria that generates command
-    search: str
-        where [search]
-        criteria for your search
-    whatwant: str
-        select [whatwant]
-        column (set of values) you want out.
-
-    Returns
-    -------
-    exec_str: str
-        sqlite query command.
-    """
-
-    exec_str = ('select ' + whatwant + ' from resources inner join transactions\
-                on transactions.resourceid = resources.resourceid where '
-                + str(search) + ' = ' + str(array[0]))
-
-    for ar in array[1:]:
-        exec_str += ' or ' + str(ar)
-
-    return exec_str
-
-
-def isotope_calc(wasteid_array, snf_inventory, cursor):
-    """ Calculates isotope mass using mass fraction in compositions table.
-
-        Fetches all from compositions table.
-        Compositions table has the following format:
-            SimId / QualId / NucId / MassFrac
-        Then sees if the qualid matches, and if it multiplies
-        the mass fraction by the snf_inventory.
-
-    Prameters
-    ---------
-    wasteid_array: array
-        array of qualid of wastes
-    snf_inventory: float
-        total mass of snf
-    cursor: cursor
-        cursor for sqlite3
-
-    Returns
-    -------
-    nuclide_inven: array
-        inventory of individual nuclides.
-    """
-
-    # Get compositions of different commodities
-    # SimId / QualId / NucId / MassFrac
-    cur = cursor
-    comp = cur.execute('select * from compositions').fetchall()
-
-    nuclide_inven = ['total snf inventory = ' + str(snf_inventory) + 'kg']
-    # if the 'qualid's match,
-    # the nuclide quantity and calculated and displayed.
-    for isotope in comp:
-        for num in wasteid_array:
-            if num == isotope[1]:
-                nuclide_quantity = str(snf_inventory*isotope[3])
-                nuclide_name = str(nucname.name(isotope[2]))
-                nuclide_inven.append(nuclide_name + ' = ' + nuclide_quantity)
-
-    return nuclide_inven
 
 
 if __name__ == "__main__":
