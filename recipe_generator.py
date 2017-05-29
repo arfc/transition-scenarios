e4091e9217db88e14199a4007500a0ab52a87b7f
gyutaepark <gtpark96@gmail.com>
2017-05-29 15:29:15 -0500

    Created a new python script for making Cycamore Reactor inputs

---
+++ recipe_generator.py
@@ -0,0 +1,9 @@
+# Imports a tab delimited data file containing
+# reactor data to output formatted input for
+# Cycamore Reactors.
+import csv
+import jinja2
+import sys
+
+# Check number of input arguments
+#
