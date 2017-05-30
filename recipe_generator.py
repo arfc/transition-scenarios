31362954822d58cb00ad4995be82c49a0474391a
gyutaepark <gtpark96@gmail.com>
2017-05-30 00:11:37 -0500

    Updated recipe generator. Now functional without Reactor output composition

--- recipe_generator.py
+++ recipe_generator.py
@@ -6,4 +6,49 @@ import jinja2
 import sys
 
 # Check number of input arguments
-#
+if len(sys.argv) < 2:
+    print('Usage: python recipe_generator.py [InputFileName] [TemplateName]')
+
+def import_csv(in_csv):
+    with open(in_csv, 'r') as source:
+        sourcereader = csv.reader(source, delimiter='\t')
+        data_list = []
+        for row in sourcereader:
+            data_list.append(row)
+
+    return data_list;
+
+def import_template(in_template):
+    with open(in_template, 'r') as default:
+        output_template = jinja2.Template(default.read())
+
+    return output_template;
+
+def make_recipe(in_list, in_template):
+    col_max = len(in_list[0]) - 1
+    row_max = len(in_list) - 1
+
+    for col in range(0, col_max):
+        reactor_type = in_list[col][6]
+        batch = int(in_list[col][24])
+        assem_per_batch = 0
+        assem_no = 0
+        if reactor_type == 'BWR':
+            assem_no = 732
+            assem_per_batch = assem_no / batch
+        else:
+            assem_no = 240
+            assem_per_batch = assem_no / batch
+
+        template_new = in_template.render(name=in_list[col][0], lifetime=in_list[col][6], assem_size=in_list[col][8], n_assem_core=assem_no, n_assem_batch=assem_per_batch, power_cap=in_list[col][1])
+        with open(in_list[col][0].replace(' ', '_') + '.xml', 'w') as output:
+            output.write(template_new)
+
+def main(in_csv, in_template):
+    data_list = import_csv(in_csv)
+    input_temp = import_template(in_template)
+    make_recipe(data_list, input_temp)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
