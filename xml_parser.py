4f9b37e5a3b8aaaa9065bade2d9721ed69c2a14e
gyutaepark <gtpark96@gmail.com>
2017-05-30 21:24:20 -0500

    Edited script to run for all files in templated_output directory

--- xml_parser.py
+++ xml_parser.py
@@ -1,12 +1,7 @@
 #This script is a basic xml parser with simple xinclude support
 #using ElementTree. Mainly for convinience.
-
-import sys
+import os
 import lxml.etree as ET
-
-# Check number of input arguments
-if len(sys.argv) < 1:
-    print('Usage: python xml_parser.py [XML.xml]')
 
 def parse_xml(in_xml):
     tree = ET.parse(in_xml)
@@ -16,8 +11,12 @@ def parse_xml(in_xml):
     with open(in_xml, 'wb') as output:
         output.write(ET.tostring(root, pretty_print=True))
 
-def main(in_xml):
-    parse_xml(in_xml)
+def parse_all():
+    for filename in os.listdir('./templated_output'):
+        parse_xml('./templated_output/' + filename)
+
+def main():
+    parse_all()
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main()
