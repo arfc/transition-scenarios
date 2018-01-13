0440c77e94dc52e18601acaa05c1e323d7e0aad8
gyutaepark <gtpark96@gmail.com>
2018-01-14 01:28:56 +0900

    Created merge function (in progress)

--- import_data/merge_coordinates.py
+++ import_data/merge_coordinates.py
@@ -5,4 +5,22 @@ sys.path.append('./../analysis')
 import analysis as an
 import import_pris as ip
 import csv
-import fuzzywuzzy
+from fuzzywuzzy import fuzz
+import sqlite3 as sql
+
+
+def merge_coordinates(pris, scrape):
+    with open(pris, encoding='utf-8') as src:
+        reader = csv.reader(src, delimiter=',')
+        countries = sorted({row[0] for row in reader})
+        country_list = [w.replace('US', 'United States of America')
+                        for w in countries]
+        country_list = [w.replace('China', "People's Republic of China")
+                        for w in countries]
+        for cnt in countries:
+            cur = an.get_cursor(pris)
+            coords = cur.execute(
+                "SELECT name, long, lat FROM reactors_coordinates "
+                "WHERE country = " + cnt + " COLLATE NOCASE")
+
+    return reader
