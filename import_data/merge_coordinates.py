c75efc4daf3f04a80d855283f755b6107030bb8a
gyutaepark <gtpark96@gmail.com>
2018-01-21 13:22:28 -0600

    Completed script

--- import_data/merge_coordinates.py
+++ import_data/merge_coordinates.py
@@ -1,28 +1,39 @@
-import sys
-sys.path.append('./..')
-sys.path.append('./../analysis')
-
-import analysis as an
-import import_pris as ip
 import csv
 from fuzzywuzzy import fuzz
 import sqlite3 as sql
 
 
-def merge_coordinates(pris, scrape):
-	pris = []
-    with open(pris, encoding='utf-8') as src:
+def get_cursor(file_name):
+    """ Connects and returns a cursor to an sqlite output file
+
+    Parameters
+    ----------
+    file_name: str
+        name of the sqlite file
+
+    Returns
+    -------
+    sqlite cursor3
+    """
+    con = sql.connect(file_name)
+    con.row_factory = sql.Row
+    return con.cursor()
+
+
+def merge_coordinates(pris_link, scrape):
+    pris = []
+    with open(pris_link, encoding='utf-8') as src:
         reader = csv.reader(src, delimiter=',')
         pris.extend(reader)
-        cur = an.get_cursor(pris)
-        coords = cur.execute("SELECT name, long, lat FROM reactors_coordinates")
-        for web, prs in zip(coords, pris):
-            if fuzz.partial_ratio(web['name'].lower(),prs[1].lower()) > 80:
-            	prs[13] = double(web['long'])
-            	prs[14] = double(web['lat'])
-                	
-                
-    return reader
-
-
-# [13]: Long [14]: Lat
+        cur = get_cursor(scrape)
+        coords = cur.execute(
+            "SELECT name, long, lat FROM reactors_coordinates")
+        for web in coords:
+            for prs in pris[1:]:
+                if fuzz.partial_ratio(web['name'].lower(),
+                                      prs[1].lower()) > 70:
+                    prs[13] = web['lat']
+                    prs[14] = web['long']
+    with open('pris_coordinates.csv', 'w') as output:
+        writer = csv.writer(output, delimiter=',')
+        writer.writerows(pris)
