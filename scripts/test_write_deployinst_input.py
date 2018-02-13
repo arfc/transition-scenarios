c765aa055639954a0cb056b9a7f2081ff4c2f016
Jin Whan Bae <jbae11@illinois.edu>
2018-02-12 20:51:12 -0600

    write_input test init

---
+++ scripts/test_write_deployinst_input.py
@@ -0,0 +1,10 @@
+import unittest
+import analysis as an
+import numpy as np
+import collections
+
+
+
+class WriteDeployinstTest(unittest.TestCase):
+    """ Tests for write_deployinst_input.py """
+    
