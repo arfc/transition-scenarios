eca9020cb374aa8ad9241fd008544f1bfa8431b9
Jin Whan Bae <jbae11@illinois.edu>
2018-02-22 16:27:32 -0600

    input and output switched

--- scripts/random_lifetime_extension.py
+++ scripts/random_lifetime_extension.py
@@ -5,31 +5,47 @@ import random
     finds the DeployInst block for `France_government`,
     and extends the lifetime of LWR reactors (with original lifetime 720)
     according to a Gaussian distribution (mean 10, sd 3 [years])"""
-trig = False
-trig2 = False
-# these values are hard-coded for ease of use
-new_file = open(
-    '../2017-bae-europe/eu_future/randomly_extended_french_lwrs.xml', 'w')
-default_file = open('../2017-bae-europe/eu_future/default_input_file.xml', 'r')
+def generate_input(input_path, output_path, orig_lifetime, country):
+    """ This function takes a full CYCLUS input xml file,
+        finds the DeployInst block for country,
+        and extends the lifetime of LWR reactors 
+        according to a Gaussian distribution (mean 10, sd 3 [years])
 
-for line in default_file:
-    if 'France_government' in line:
-        trig = True
-    if trig and 'lifetime' in line:
-        trig2 = True
-    if trig2:
-        if '</lifetimes>' in line:
-            trig2 = False
-            trig = False
-        else:
-            if ('<val>720</val>' in line):
-                # this is where we add the random
-                lifetime_extension = abs(int(np.random.normal(10, 3)))
-                print(lifetime_extension)
-                lifetime = 720 + lifetime_extension * 12
-                new_file.write('<val>%i</val>\n' % lifetime)
-                continue
-    new_file.write(line)
+        Parameters
+        ----------
+        input_path: str
+            path for file to be copied and modified
+        output_path: str
+            path for the modified file
+        orig_lifetime: int
+            original lifetime for lwrs (to search for)
+        country: str
+            country (region) to make this change in
+        """
+    trig = False
+    trig2 = False
+    # these values are hard-coded for ease of use
+    new_file = open(output_path, 'w')
+    default_file = open(input_path, 'r')
 
-new_file.close()
-default_file.close()
+    for line in default_file:
+        if country in line:
+            trig = True
+        if trig and 'lifetime' in line:
+            trig2 = True
+        if trig2:
+            if '</lifetimes>' in line:
+                trig2 = False
+                trig = False
+            else:
+                if ('<val>%i</val>' %orig_lifetime in line):
+                    # this is where we add the random
+                    lifetime_extension = abs(int(np.random.normal(10, 3)))
+                    print(lifetime_extension)
+                    lifetime = 720 + lifetime_extension * 12
+                    new_file.write('<val>%i</val>\n' % lifetime)
+                    continue
+        new_file.write(line)
+
+    new_file.close()
+    default_file.close()
