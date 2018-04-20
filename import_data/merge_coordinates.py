0188ceb298fe45fbd16a65a7f05f9c63e9a7b426
gyutaepark <gtpark96@gmail.com>
2018-04-20 17:16:00 -0500

    removed modified.csv; updated merge script for readability and style; added updated pris database

--- import_data/merge_coordinates.py
+++ import_data/merge_coordinates.py
@@ -1,7 +1,11 @@
-import csv
 from fuzzywuzzy import fuzz
+import numpy as np
+import pandas as pd
 import sqlite3 as sql
 import sys
+
+if len(sys.argv) < 3:
+    print('Usage: python merge_coordinates.py [pris_link] [webscrape_link]')
 
 
 def get_cursor(file_name):
@@ -21,8 +25,24 @@ def get_cursor(file_name):
     return con.cursor()
 
 
-def merge_coordinates(pris_link, scrape):
-    pris = []
+def import_pris(pris_link):
+    pris = pd.read_csv(pris_link,
+                       delimiter=',',
+                       encoding='iso-8859-1'
+                       )
+    pris.insert(13, 'Latitude', np.nan)
+    pris.insert(14, 'Longitude', np.nan)
+    pris = pris.replace(np.nan, '')
+    return pris
+
+
+def import_webscrape_data(scrape_link):
+    cur = get_cursor(scrape_link)
+    coords = cur.execute("SELECT name, long, lat FROM reactors_coordinates")
+    return coords
+
+
+def get_edge_cases():
     others = {'OHI-': 'Ōi',
               'ASCO-': 'Ascó',
               'ROVNO-': 'Rivne',
@@ -40,43 +60,59 @@ def merge_coordinates(pris_link, scrape):
               'SUMMER-': 'Virgil C. Summer',
               'FARLEY-': 'Joseph M. Farley',
               'ST. LAURENT ': 'Saint-Laurent',
-              'HADDAM NECK': 'Connecticut1 Yankee',
+              'HADDAM NECK': 'Connecticut Yankee',
               'HIGASHI DORI-1 (TOHOKU)': 'Higashidōri',
               }
+    return others
+
+
+def sanitize_webscrape_name(name):
     blacklist = ['nuclear', 'power',
                  'plant', 'generating',
                  'station', 'reactor', 'atomic',
                  'energy', 'center', 'electric']
-    with open(pris_link, encoding='utf-8') as src:
-        reader = csv.reader(src, delimiter=',')
-        pris.extend(reader)
-        cur = get_cursor(scrape)
-        coords = cur.execute(
-            "SELECT name, long, lat FROM reactors_coordinates")
-        for web in coords:
-            for prs in pris[1:]:
-                name_web = web['name'].lower()
-                for blacklisted in blacklist:
-                    name_web = name_web.replace(blacklisted, '')
-                if fuzz.ratio(name_web.rstrip(), prs[1].lower()) > 64:
-                    # print(name_web.rstrip(), ' and ', prs[1].lower())
-                    prs[13] = web['lat']
-                    prs[14] = web['long']
-                else:
-                    for other in others.keys():
-                        if fuzz.ratio(prs[1].lower(), other.lower()) > 80:
-                            if fuzz.ratio(others[other].lower(),
-                                          name_web.rstrip().lower()) > 75:
-                                # print(prs[1], ' and ', name_web.rstrip())
-                                prs[13] = web['lat']
-                                prs[14] = web['long']
-    with open('reactors_pris_2016.csv', 'w') as output:
-        writer = csv.writer(output, delimiter=',')
-        writer.writerows(pris)
+    name = name.lower()
+    for blacklisted in blacklist:
+        name = name.replace(blacklisted, '')
+    name = name.strip()
+    name = ' '.join(name.split())
+    return name
 
 
-def main(pris, scrape):
-    merge_coordinates(pris, scrape)
+def merge_coordinates(pris_link, scrape_link):
+    others = get_edge_cases()
+    pris = import_pris(pris_link)
+    coords = import_webscrape_data(scrape_link)
+    for web in coords:
+        for idx, prs in pris.iterrows():
+            webscrape_name = sanitize_webscrape_name(web['name'])
+            pris_name = prs[1].lower()
+            if fuzz.ratio(webscrape_name, pris_name) > 64:
+                prs[13] = web['lat']
+                prs[14] = web['long']
+            else:
+                for other in others.keys():
+                    edge_case_key = other.lower()
+                    edge_case_value = others[other].lower()
+                    if (fuzz.ratio(pris_name, edge_case_key) > 80 and
+                            fuzz.ratio(webscrape_name, edge_case_value) > 75):
+                        prs[13] = web['lat']
+                        prs[14] = web['long']
+    return pris
+
+
+def save_output(pris):
+    pris.to_csv('reactors_pris_2016.csv',
+                index=False,
+                sep=',',
+                )
+
+
+def main(pris_link, scrape_link):
+    pris = merge_coordinates(pris_link, scrape_link)
+    save_output(pris)
+
 
 if __name__ == "__main__":
+    # print(import_pris(sys.argv[1]))
     main(sys.argv[1], sys.argv[2])
