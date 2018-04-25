d9b62c0ad4977f8b569295a36eafe53b1f4f4bc4
PEP8 Speaks <pep8speaks@gmail.com>
2018-04-26 04:42:32 +0530

    Fix pep8 errors in scripts/tests/test_analysis.py

--- scripts/tests/test_analysis.py
+++ scripts/tests/test_analysis.py
@@ -11,6 +11,7 @@ import analysis as an
 dir = os.path.dirname(__file__)
 test_sqlite_path = os.path.join(dir, 'test.sqlite')
 
+
 def get_sqlite():
     con = lite.connect(test_sqlite_path)
     con.row_factory = lite.Row
