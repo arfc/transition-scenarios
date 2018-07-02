a7694ba6a20b591181af488914bd440f1cacce98
jbae11@illinois.edu <jbae11@illinois.edu>
2018-07-02 07:19:28 -0400

    Runall input

--- input/udb_transition/run_all_input.py
+++ input/udb_transition/run_all_input.py
@@ -4,14 +4,17 @@ import os
 folders = [x[0] for x in os.walk('.')]
 folders = [x for x in folders if 'growth' in x]
 
-for directory in folders:
-    print('RUNNING %s:' %directory[2:])
-    rm_command = 'rm %s/output.sqlite' %directory
-    bash_command = 'cyclus %s/compiled_input.xml -o %s/output.sqlite --warn-limit 0' %(directory, directory)
-    lint_command = 'xmllint --xinclude -o %s/compiled_input.xml %s/input.xml' %(directory,directory)
-    rm = subprocess.Popen(rm_command.split(), stdout=subprocess.PIPE)
-    lint = subprocess.Popen(lint_command.split(), stdout=subprocess.PIPE)
-    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    print(output)
-    print(error)
+with open('log.txt') as f: 
+    for directory in folders:
+        print('RUNNING %s:' %directory[2:])
+        if os.path.isfile('%s/output.sqlite' %directory):
+            f.write('%s IS ALREADY THERE' %directory)
+            continue
+        bash_command = 'cyclus %s/compiled_input.xml -o %s/output.sqlite --warn-limit 0' %(directory, directory)
+        lint_command = 'xmllint --xinclude -o %s/compiled_input.xml %s/input.xml' %(directory,directory)
+        lint = subprocess.Popen(lint_command.split(), stdout=subprocess.PIPE)
+        process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+        f.write('FINISHED %s' %directory)
+        output, error = process.communicate()
+        print(output)
+        print(error)
