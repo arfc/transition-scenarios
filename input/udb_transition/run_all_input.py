6d9cc4eed5f046c62382e9219b162cde19082d0f
jbae11@illinois.edu <jbae11@illinois.edu>
2018-07-09 09:16:10 -0400

    run all input

--- input/udb_transition/run_all_input.py
+++ input/udb_transition/run_all_input.py
@@ -3,13 +3,16 @@ import os
 
 folders = [x[0] for x in os.walk('.')]
 folders = [x for x in folders if 'growth' in x]
-
+restart = True
 with open('log.txt', 'w') as f: 
     for directory in folders:
         print('RUNNING %s:' %directory[2:])
         if os.path.isfile('%s/output.sqlite' %directory):
-            f.write('%s IS ALREADY THERE' %directory)
-            continue
+            if restart:
+                os.remove('%s/output.sqlite' %directory)
+            else:
+                f.write('%s IS ALREADY THERE' %directory)
+                continue
         bash_command = 'cyclus %s/compiled_input.xml -o %s/output.sqlite --warn-limit 0' %(directory, directory)
         lint_command = 'xmllint --xinclude -o %s/compiled_input.xml %s/input.xml' %(directory,directory)
         lint = subprocess.Popen(lint_command.split(), stdout=subprocess.PIPE)
