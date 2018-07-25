10f19666a915dc0e365decbc142cb370505535ff
jbae11 <jbae11@illinois.edu>
2018-07-25 14:46:40 -0500

    running all input

--- input/udb_transition/run_all_input.py
+++ input/udb_transition/run_all_input.py
@@ -7,19 +7,27 @@ folders = [x for x in folders if 'growth' in x]
 restart = True
 for directory in folders:
     print('RUNNING %s:' %directory[2:])
-    if os.path.isfile('%s/output.sqlite' %directory):
-        if restart:
-            os.remove('%s/output.sqlite' %directory)
-        else:
-            f.write('%s IS ALREADY THERE' %directory)
-            continue
+        os.remove('%s/*.sqlite' %directory)
     # copy blanket file
-    copyfile('%s/sfr_dep.xml' %directory, '%s/blanket_dep.xml' %directory)
-    bash_command = 'cyclus %s/compiled_input.xml -o %s/output.sqlite --warn-limit 0' %(directory, directory)
-    lint_command = 'xmllint --xinclude -o %s/compiled_input.xml %s/input.xml' %(directory,directory)
+    print('RUNNING PRECISE')
+    bash_command = 'cyclus %s/compiled_precise.xml -o %s/precise.sqlite --warn-limit 0' %(directory, directory)
+    lint_command = 'xmllint --xinclude -o %s/compiled_precise.xml %s/udb_precise.xml' %(directory,directory)
     lint = subprocess.Popen(lint_command.split(), stdout=subprocess.PIPE)
     process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-    f.write('FINISHED %s' %directory)
     output, error = process.communicate()
-    print(output)
     print(error)
+
+    print('RUNNING RECIPE')
+    bash_command = 'cyclus %s/compiled_recipe.xml -o %s/recipe.sqlite --warn-limit 0' %(directory, directory)
+    lint_command = 'xmllint --xinclude -o %s/compiled_recipe.xml %s/udb_recipe.xml' %(directory,directory)
+    lint = subprocess.Popen(lint_command.split(), stdout=subprocess.PIPE)
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+    print('RUNNING NO')
+    bash_command = 'cyclus %s/compiled_no.xml -o %s/no.sqlite --warn-limit 0' %(directory, directory)
+    lint_command = 'xmllint --xinclude -o %s/compiled_no.xml %s/udb_no.xml' %(directory,directory)
+    lint = subprocess.Popen(lint_command.split(), stdout=subprocess.PIPE)
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
