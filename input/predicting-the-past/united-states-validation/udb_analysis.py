5e7f6244928af297f7d25d9fd430b3ae323c284e
gwenchee <gwendolynchee95@gmail.com>
2018-11-19 14:56:51 -0500

    updates for decayheat plots for ans winter

--- input/predicting-the-past/united-states-validation/udb_analysis.py
+++ input/predicting-the-past/united-states-validation/udb_analysis.py
@@ -53,3 +53,34 @@ def udb_analysis(dat_file):
     curie_isotope_list = [x[1] for x in isotope_list]
 
     return spent_fuel_mass_cum, CURIE_isotope_cum_all, curie_isotope_list
+
+
+def udb_analysis_bu(dat_file):
+    # Import CURIE data from dat file into a dataframe
+    # (deleted the heading of the dat file)
+    names = [
+        'assembly_id',
+        'reactor_id',
+        'reactor_type',
+        'initial_uranium_kg',
+        'initial_enrichment',
+        'discharge_burnup',
+        'discharge_date',
+        'discharge_time',
+        'total_assembly_decay_heat_kw',
+        'name',
+        'evaluation_date',
+        'total_mass_g',
+        'total_radioactivity_curies']
+    df = pd.read_csv(dat_file,
+                     sep='\s+',
+                     index_col=False,
+                     names=names)
+
+    # Rearrange dataframe to only keep relevant columns
+    burnup_everything = df[['discharge_date', 'discharge_burnup']]
+
+    # Rearrange dataframe to average spent fuel burn up by discharge date
+    burn_up_grouped = burnup_everything.groupby('discharge_date').mean()
+
+    return burn_up_grouped
