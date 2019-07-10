dc943d86a4c54978ca989176aabc1ed29259b353
robfairh <ref3@illinois.edu>
2019-07-10 15:29:47 -0500

    pepito

--- input/eg01-eg30/plot_flatpower_nond3ploy.py
+++ input/eg01-eg30/plot_flatpower_nond3ploy.py
@@ -88,4 +88,3 @@ for commod in back_commods:
     plotter.plot_demand_supply_nond3ploy(all_dict[commod],
                                          agent_entry_dict[commod],
                                          commod, name, False, True, 1)
-    
