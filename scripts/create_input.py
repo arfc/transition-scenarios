8831068b1e680b735b98354ad1cf7733cc806ac4
abachma2 <abachma2@vols.utk.edu>
2021-01-11 08:35:10 -0600

    pulled user specifications to beginning

--- scripts/create_input.py
+++ scripts/create_input.py
@@ -4,11 +4,10 @@ import sys
 import predicting_the_past_import as import_data
 
 
-#use of file: python create_input.py  2019 united_states
-#asumes file name and location, input year of data and region to be used
-
-data_year = sys.argv[1]
-region = sys.argv[2]
+data_year = 2019
+start_year = 1965
+region = 'united_states'
+project = 'haleu'
 
 #read in csv, remove first 2 columns
 reactor_data = import_data.import_pris('../database/Year-end Reactor Status_' + str(data_year) +'.csv')
@@ -22,8 +21,8 @@ import_data.save_output(reactor_data, data_year)
 pris_file = '../database/reactors_pris_' + str(data_year) + '.csv'
 deployinst_tmpl = '../input/predicting-the-past/templates/' + region + '/deployinst_template.xml'
 inclusions_tmpl = '../input/predicting-the-past/templates/inclusions_template.xml'
-deployment_path = '../input/haleu/inputs/' + region + '/buildtimes'
-reactor_path = '../input/haleu/inputs/' + region + '/reactors'
+deployment_path = '../input/' + project + '/inputs/' + region + '/buildtimes'
+reactor_path = '../input/' + project + '/inputs/' + region + '/reactors'
 reactor_template = '../input/predicting-the-past/templates/reactors_template.xml'
 
 
@@ -44,8 +43,8 @@ import_data.write_recipes(fresh51, spent51, recipe_template, 100, region)
 
 reactor_list = import_data.select_region(pris, region)
 import_data.write_reactors(reactor_list, reactor_path, reactor_template,18,1) # change cycle and refuel time 
-buildtime = import_data.deploy_reactors(pris_file, region, 1965, deployinst_tmpl,
+buildtime = import_data.deploy_reactors(pris_file, region, start_year, deployinst_tmpl,
                             inclusions_tmpl, reactor_path, deployment_path)
 
 cyclus_tmpl = ('../input/predicting-the-past/templates/' + region + '/' + region + '_template.xml')
-import_data.render_cyclus(cyclus_tmpl, region, buildtime, '../input/haleu/inputs/',51) # change burn up 
+import_data.render_cyclus(cyclus_tmpl, region, buildtime, '../input/' + project + '/inputs/',51) # change burn up 
