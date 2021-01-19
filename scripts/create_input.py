8813d6b931bb8831d810a39e38a01e3a4765730d
abachma2 <amandab7@illinois.edu>
2021-01-19 17:08:01 -0600

    changed file paths for templates

--- scripts/create_input.py
+++ scripts/create_input.py
@@ -4,8 +4,8 @@ import sys
 import predicting_the_past_import as import_data
 
 # user defined characteristics of cyclus simulation
-data_year = 2019
-start_year = 1965
+data_year = 2020
+start_year = 1985
 region = 'united_states'
 project = 'haleu'
 
@@ -16,13 +16,13 @@ import_data.merge_coordinates('../database/Year-end Reactor Status_' + str(data_
 pris_file = '../database/reactors_pris_' + str(data_year) + '.csv'
 recipes = import_data.import_csv('../database/vision_recipes/uox.csv', ',')
 
-deployinst_tmpl = '../input/predicting-the-past/templates/' + \
+deployinst_tmpl = '../templates/' + \
     region + '/deployinst_template.xml'
-inclusions_tmpl = '../input/predicting-the-past/templates/inclusions_template.xml'
+inclusions_tmpl = '../templates/inclusions_template.xml'
 recipe_template = import_data.load_template(
-    '../input/predicting-the-past/templates/recipes_template.xml')
-reactor_template = '../input/predicting-the-past/templates/reactors_template.xml'
-cyclus_tmpl = ('../input/predicting-the-past/templates/' +
+    '../templates/recipes_template.xml')
+reactor_template = '../templates/reactors_template.xml'
+cyclus_tmpl = ('../templates/' +
                region + '/' + region + '_template.xml')
 
 recipe_path = '../input/' + project + '/inputs/' + region + '/recipes/'
@@ -39,11 +39,11 @@ for bu in burnups:
     spent = import_data.get_composition_spent(recipes, bu)
     import_data.write_recipes(fresh, spent, recipe_template, bu, recipe_path)
 
-reactor_list = import_data.select_region(pris, region)
+reactor_list = import_data.select_region(pris, region, start_year)
 import_data.write_reactors(
     reactor_list,
     reactor_path,
-    reactor_template,
+    reactor_template, start_year,
     18,
     1)  # change cycle and refuel time
 buildtime = import_data.deploy_reactors(pris_file, region, start_year, deployinst_tmpl,
