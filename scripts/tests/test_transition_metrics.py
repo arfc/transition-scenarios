18ec688beac52c32811fc774be8f0ac7955636b6
abachma2 <amandab7@illinois.edu>
2021-10-08 10:23:26 -0500

    added test for add_year

--- scripts/tests/test_transition_metrics.py
+++ scripts/tests/test_transition_metrics.py
@@ -16,7 +16,7 @@ class test_reading_output(object):
 
     def test_rx_commission_decommission1(self):
         exp = pd.Series(data={0:0.0, 1:0.0, 
-        2:1.0, 3:2.0, 4:2.0, 5:2.0, 6:2.0}, name='lwr_total')
+            2:1.0, 3:2.0, 4:2.0, 5:2.0, 6:2.0}, name='lwr_total')
         non_lwr = ['United States', 'FuelCycle', 'FuelSupply', 
            'Repository', 'UNITED_STATES_OF_AMERICA', 
            'Reactor_type1_enter', 'Reactor_type1_exit']
@@ -26,7 +26,7 @@ class test_reading_output(object):
 
     def test_rx_commission_decommission2(self):
         exp = pd.Series(data={0:0.0, 1:0.0, 
-        2:1.0, 3:2.0, 4:2.0, 5:2.0, 6:2.0}, name='lwr_total')
+            2:1.0, 3:2.0, 4:2.0, 5:2.0, 6:2.0}, name='lwr_total')
         non_lwr = ['United States', 'FuelCycle', 'FuelSupply', 
            'Repository', 'UNITED_STATES_OF_AMERICA', 
            'Reactor_type1']
@@ -34,6 +34,14 @@ class test_reading_output(object):
         obs = df['lwr_total']
         assert_series_equal(exp,obs)
         
+def test_add_year():
+    exp = pd.Series(data={0:1965.08, 1: 1965.33, 2: 1965.58, 3:1965.83,
+    4:1966.08}, name='Year')
+    data = np.array([(1,2,3),(4,5,6), (7,8,9), (10,11,12), (13, 14, 15)])
+    df = pd.DataFrame(data, columns=['Time', 'Data1', 'Data2'])
+    obs = tm.add_year(df)
+    assert_series_equal(exp, obs['Year'])
+
 def test_calculate_feed1():
     exp = 10
     product = 5
