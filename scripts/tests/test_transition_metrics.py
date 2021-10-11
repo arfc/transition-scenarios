b87c4665619150fbd56f70e07b530409c9a0c1e8
abachma2 <amandab7@illinois.edu>
2021-10-11 13:17:01 -0500

    wrote sum_and_add_missing_time, pulled functions out of class

--- scripts/tests/test_transition_metrics.py
+++ scripts/tests/test_transition_metrics.py
@@ -1,50 +1,44 @@
 import numpy as np
 import pandas as pd
+from uuid import UUID
 from pandas._testing import assert_series_equal
 from pandas._testing import assert_frame_equal
 
 import transition_metrics as tm
 
-class test_reading_output(object):
+class file_df_info(object):
     def __init__(self):
         self.output_file1 = 'transition_metrics_decommission_test.sqlite'
         self.output_file2 = 'transition_metrics_nodecommission_test.sqlite'
+        self.test_df = pd.DataFrame(data={'Time': [0,1,1,3], 
+        'Quantity':[2,5,6,8]})
 
-    def test_get_metrics(self):
-         
-        obs = 'Test_case' #tm.get_metrics(self.output_file)
-        assert isinstance(obs, str)
+def test_get_metrics():     
+    obs = 'Test_case' #tm.get_metrics(self.output_file)
+    assert isinstance(obs, str)
 
-    def test_rx_commission_decommission1(self):
-        exp = pd.Series(data={0:0.0, 1:0.0, 
-            2:1.0, 3:2.0, 4:2.0, 5:2.0, 6:2.0}, name='lwr_total')
-        non_lwr = ['United States', 'FuelCycle', 'FuelSupply', 
-           'Repository', 'UNITED_STATES_OF_AMERICA', 
-           'Reactor_type1_enter', 'Reactor_type1_exit']
-        df = tm.rx_commission_decommission(self.output_file1, non_lwr)
-        obs = df['lwr_total']
-        assert_series_equal(exp,obs)
+def test_rx_commission_decommission1():
+    exp = pd.Series(data={0:0.0, 1:0.0, 
+        2:1.0, 3:2.0, 4:2.0, 5:2.0, 6:2.0}, name='lwr_total')
+    non_lwr = ['United States', 'FuelCycle', 'FuelSupply', 
+        'Repository', 'UNITED_STATES_OF_AMERICA', 
+        'Reactor_type1_enter', 'Reactor_type1_exit']
+    file_info = file_df_info()
+    df = tm.rx_commission_decommission(file_info.output_file1, non_lwr)
+    obs = df['lwr_total']
+    assert_series_equal(exp,obs)
 
-    def test_rx_commission_decommission2(self):
-        exp = pd.Series(data={0:0.0, 1:0.0, 
-            2:1.0, 3:2.0, 4:2.0, 5:2.0, 6:2.0}, name='lwr_total')
-        non_lwr = ['United States', 'FuelCycle', 'FuelSupply', 
-           'Repository', 'UNITED_STATES_OF_AMERICA', 
-           'Reactor_type1']
-        df = tm.rx_commission_decommission(self.output_file2, non_lwr)
-        obs = df['lwr_total']
-        assert_series_equal(exp,obs)
+def test_rx_commission_decommission2():
+    exp = pd.Series(data={0:0.0, 1:0.0, 
+        2:1.0, 3:2.0, 4:2.0, 5:2.0, 6:2.0}, name='lwr_total')
+    non_lwr = ['United States', 'FuelCycle', 'FuelSupply', 
+        'Repository', 'UNITED_STATES_OF_AMERICA', 
+        'Reactor_type1']
+    file_info = file_df_info()
+    df = tm.rx_commission_decommission(file_info.output_file2, non_lwr)
+    obs = df['lwr_total']
+    assert_series_equal(exp,obs)
 
-    def test_get_transactions(self):
-        exp = pd.DataFrame(data = {'Time': [0,1,1], 'SimId':[0, 
-        '6af6d305-e3be-4790-8920-b3f3bec3d6f7', '6af6d305-e3be-4790-8920-b3f3bec3d6f7'],
-        'TransactionId':[0.0, 0.0, 1.0], 'ResourceId':[0.0, 10.0, 20.0],
-        'ObjId':[0.0, 9.0, 10.0], 'SenderId':[0.0, 21.0, 21.0], 
-        'ReceiverId':[0.0, 24.0, 24.0], 'Commodity':[0, 'fresh_uox', 'fresho_uox'],
-        'Units':[0, 'kg', 'kg'], 'Quantity':[0.0, 33000.0, 33000.0]})
-        obs = tm.get_transactions(self.output_file1)
-        assert_frame_equal(exp, obs[0:3], check_dtype=False)
-        
 def test_add_year():
     exp = pd.Series(data={0:1965.08, 1: 1965.33, 2: 1965.58, 3:1965.83,
     4:1966.08}, name='Year')
@@ -52,6 +46,23 @@ def test_add_year():
     df = pd.DataFrame(data, columns=['Time', 'Data1', 'Data2'])
     obs = tm.add_year(df)
     assert_series_equal(exp, obs['Year'])
+
+def test_get_transactions():
+    exp = pd.DataFrame(data = {'Time': [0,1,1], 'SimId':[0, 
+        UUID('6af6d305-e3be-4790-8920-b3f3bec3d6f7'), UUID('6af6d305-e3be-4790-8920-b3f3bec3d6f7')],
+        'TransactionId':[0.0, 0.0, 1.0], 'ResourceId':[0.0, 10.0, 12.0],
+        'ObjId':[0.0, 9.0, 10.0], 'SenderId':[0.0, 21.0, 21.0], 
+        'ReceiverId':[0.0, 24.0, 24.0], 'Commodity':[0, 'fresh_uox', 'fresh_uox'],
+        'Units':[0, 'kg', 'kg'], 'Quantity':[0.0, 33000.0, 33000.0]})
+    file_info = file_df_info()
+    obs = tm.get_transactions(file_info.output_file1)
+    assert_frame_equal(exp, obs[0:3])
+        
+def test_sum_and_add_missing_time():
+    exp = pd.DataFrame(data={'Time':[0,1,2,3], 'Quantity':[2.0,11.0, 0.0, 8.0]})
+    test_df = file_df_info()
+    obs = tm.sum_and_add_missing_time(test_df.test_df)
+    assert_frame_equal(exp, obs[0:4])
 
 def test_calculate_feed1():
     exp = 10
