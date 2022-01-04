aff7aa543bf841645ee26d46c7866eb6709561d7
abachma2 <amandab7@illinois.edu>
2022-01-04 14:21:48 -0600

    made into a loop to make csv files for all transitions at once

--- input/haleu/analysis/df_to_csv.py
+++ input/haleu/analysis/df_to_csv.py
@@ -2,8 +2,12 @@ import pandas as pd
 
 import cymetric as cm
 import transition_metrics as tm
+reactors = ['xe100','mmr']
+scenarios = ['nogrowth','1percent']
 
-outfile = '../outputs/xe100_1percent.sqlite'
+for reactor in reactors:
+    for scenario in scenarios:
+        outfile = f'../outputs/{reactor}_{scenario}.sqlite'
 
-transactions = tm.add_receiver_prototype(outfile)
-transactions.to_csv('xe_1percent_transactions.csv')
+        transactions = tm.add_receiver_prototype(outfile)
+        transactions.to_csv(f'{reactor}_{scenario}__transactions.csv')
