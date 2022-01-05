23e8461c941d9473738da20022ea2560b2dd3600
abachma2 <amandab7@illinois.edu>
2022-01-05 10:13:36 -0600

    added functions to split up if I want to loop or just do one case

--- input/haleu/analysis/df_to_csv.py
+++ input/haleu/analysis/df_to_csv.py
@@ -2,12 +2,24 @@ import pandas as pd
 
 import cymetric as cm
 import transition_metrics as tm
-reactors = ['xe100','mmr']
-scenarios = ['nogrowth','1percent']
 
-for reactor in reactors:
-    for scenario in scenarios:
-        outfile = f'../outputs/{reactor}_{scenario}.sqlite'
+def loop_over_scenarios():
+    reactors = ['xe100','mmr']
+    scenarios = ['nogrowth','1percent']
 
-        transactions = tm.add_receiver_prototype(outfile)
-        transactions.to_csv(f'{reactor}_{scenario}__transactions.csv')
+    for reactor in reactors:
+        for scenario in scenarios:
+            outfile = f'../outputs/{reactor}_{scenario}.sqlite'
+
+            transactions = tm.add_receiver_prototype(outfile)
+            transactions.to_csv(f'{reactor}_{scenario}_transactions.csv')
+
+def single_scenario():
+    scenario = 'current'
+    outfile = f'../outputs/united_states_2020.sqlite'
+    transactions = tm.add_receiver_prototype(outfile)
+    transactions.to_csv(f'{scenario}_transactions.csv')
+
+
+if __name__ == '__main__':
+    single_scenario()
