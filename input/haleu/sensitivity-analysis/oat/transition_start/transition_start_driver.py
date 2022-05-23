9458795efc262f100e8df226ad243444a2daeb3c
abachma2 <amandab7@illinois.edu>
2022-05-23 11:05:30 -0500

    add driver for analysis

---
+++ input/haleu/sensitivity-analysis/oat/transition_start/transition_start_driver.py
@@ -0,0 +1,43 @@
+import sys
+import os
+from turtle import up
+sys.path.append('../../../../../scripts')
+import dakota_input as inp
+import dakota_output as oup
+# import output as oup
+import dakota.interfacing as di
+import subprocess
+# ----------------------------
+# Parse Dakota parameters file
+# ----------------------------
+
+params, results = di.read_parameters_file()
+
+# -------------------------------
+# Convert and send to Cyclus
+# -------------------------------
+
+# Edit Cyclus input file
+cyclus_template = '../../cyclus-files/scenario7.xml.in'
+scenario_name = 'ty_' + str(round(params['x1']))
+variable_dict = {'handle': scenario_name, 'start_transition': int(params['x1'])}
+# the start time needs to be an integer for cyclus
+output_xml = '../../cyclus-files/scenario7.xml'
+inp.render_input(cyclus_template, variable_dict, output_xml)
+
+# Run Cyclus with edited input file
+output_sqlite = '../cyclus-files/' + scenario_name + '.sqlite'
+os.system('rm ' + output_sqlite)
+os.system('cyclus -i ' + output_xml + ' -o ' + output_sqlite + \
+    ' --warn-limit 2')
+# ----------------------------
+# Return the results to Dakota
+# ----------------------------
+
+#for i, r in enumerate(results.responses()):
+#    if r.asv.function:
+#        r.function = 1 #can change to evaluation script
+#        print('OUT', i, r.function)
+results['enr_u'].function = oup.get_enriched_u_mass(output_sqlite, params['x1'])
+#results['des2']
+results.write()
