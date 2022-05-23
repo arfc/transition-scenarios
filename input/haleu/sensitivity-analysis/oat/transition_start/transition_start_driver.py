028f538d6eda5310885d94d6574f814a34abea20
abachma2 <amandab7@illinois.edu>
2022-05-23 17:11:47 -0500

    add response call for swu and waste (commented out)

--- input/haleu/sensitivity-analysis/oat/transition_start/transition_start_driver.py
+++ input/haleu/sensitivity-analysis/oat/transition_start/transition_start_driver.py
@@ -19,25 +19,23 @@ params, results = di.read_parameters_file()
 
 # Edit Cyclus input file
 cyclus_template = '../../cyclus-files/scenario7.xml.in'
-scenario_name = 'ty_' + str(round(params['x1']))
-variable_dict = {'handle': scenario_name, 'start_transition': int(params['x1'])}
-# the start time needs to be an integer for cyclus
+scenario_name = 'ty_' + str(round(params['ts']))
+variable_dict = {'handle': scenario_name, 'start_transition': int(params['ts'])}
 output_xml = '../../cyclus-files/scenario7.xml'
 inp.render_input(cyclus_template, variable_dict, output_xml)
 
 # Run Cyclus with edited input file
-output_sqlite = '../cyclus-files/' + scenario_name + '.sqlite'
+output_sqlite = '../../cyclus-files/' + scenario_name + '.sqlite'
 os.system('rm ' + output_sqlite)
 os.system('cyclus -i ' + output_xml + ' -o ' + output_sqlite + \
     ' --warn-limit 2')
+
 # ----------------------------
 # Return the results to Dakota
 # ----------------------------
-
-#for i, r in enumerate(results.responses()):
-#    if r.asv.function:
-#        r.function = 1 #can change to evaluation script
-#        print('OUT', i, r.function)
-results['enr_u'].function = oup.get_enriched_u_mass(output_sqlite, params['x1'])
-#results['des2']
+results['enr_u'].function = oup.get_enriched_u_mass(output_sqlite, ['Xe-100','MMR','VOYGR'],
+                                                    params['ts'])
+results['swu'].function = oup.calculate_swu(output_sqlite, ['Xe-100','MMR','VOYGR'],
+                                                    params['ts'])
+#results['waste'].function = oup.get_waste_discharge(output_sqlite, params['ts'])
 results.write()
