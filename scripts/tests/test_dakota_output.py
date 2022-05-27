87096f529f224cc0d69563dd7bd3b2b1ce1ad726
abachma2 <amandab7@illinois.edu>
2022-05-27 11:59:34 -0500

    add another df with NaN, working on merge_fillna function and tests

--- scripts/tests/test_dakota_output.py
+++ scripts/tests/test_dakota_output.py
@@ -46,15 +46,25 @@ class Test_static_info(unittest.TestCase):
                     2, 5, 6, 8, 2], 'Commodity': [
                     'fresh_uox', 'spent_uox', 'fresh_uox', 'fresh_uox', 'spent_uox'],
                 'Prototype': ['FuelCycle', 'LWR', 'Reactor_type1', 'LWR', 'Reactor_type1']})
-
+        self.test_df3 = pd.DataFrame(
+            data={
+                'Time': [
+                    0, 2, 3, 3, 5], 'Quantity': [
+                    2, 5, 6, 8, float('NaN')], 'Commodity': [
+                    'fresh_uox', 'spent_uox', 'fresh_uox', 'fresh_uox', 'spent_uox'],
+                'Prototype': ['FuelCycle', 'LWR', 'Reactor_type1', 'LWR', 'Reactor_type1']})
     def test_merge_and_fillna_col1(self):
         '''
-        Tests the function with the two default arguments supplied
+        Tests the function with the two default arguments supplied when the left 
+        DataFrame does not contain any NaN values
         '''
-        exp = pd.DataFrame(data = {'Time': [0, 3], 'Quantity':[2, 8], 
-                            'Commodity':['fresh_uox', 'fresh_uox'], 
-                            'Prototype':['FuelCyle','LWR']})
-        obs = oup.merge_and_fillna_col(self.test_df1, self.test_df2, 'Time', 'Time')
+        exp = pd.DataFrame(data={
+                'Time': [
+                    0, 1, 1, 3], 'Quantity': [
+                    2.0, 5.0, 6.0, 8.0], 'Commodity': [
+                    'fresh_uox', 'spent_uox', 'fresh_uox', 'fresh_uox'],
+                'Prototype': ['FuelCycle', 'LWR', 'Reactor_type1', 'LWR']})
+        obs = oup.merge_and_fillna_col(self.test_df1, self.test_df2, 'Quantity', 'Prototype')
         assert_frame_equal(exp, obs)
 
     def test_get_table_from_output1(self):
@@ -62,10 +72,6 @@ class Test_static_info(unittest.TestCase):
         Test function for the AgentEntry table from self.output_file1
         '''
         exp = pd.DataFrame(data={
-            #'SimId': ["b'\x17\xb1\xbe\xd5\t\x81F\x82\xa9\xbe\x05\xe6\x0erW\xcc'",
-            #"b'\x17\xb1\xbe\xd5\t\x81F\x82\xa9\xbe\x05\xe6\x0erW\xcc'",
-            #"b'\x17\xb1\xbe\xd5\t\x81F\x82\xa9\xbe\x05\xe6\x0erW\xcc'",
-            #"b'\x17\xb1\xbe\xd5\t\x81F\x82\xa9\xbe\x05\xe6\x0erW\xcc'"],
             'AgentId': [19,20,21,22],
             'Kind': ['Region', 'Inst','Facility','Facility'],
             'Spec': [':agents:NullRegion', ':agents:NullInst', ':cycamore:Source', ':cycamore:Sink'],
@@ -104,7 +110,7 @@ class Test_static_info(unittest.TestCase):
             'ParentId':[-1, 19, 20, 20],
             'Lifetime':[-1, -1, -1, -1],
             'EnterTime':[0, 0, 0, 0],
-            'ExitTime':[-1.0, -1.0, -1.0, -1.0]
+            'ExitTime':[7.0, 7.0, 7.0, 7.0]
         })
         obs = oup.create_agents_table(self.output_file1)
         assert_frame_equal(exp,obs.drop('SimId', axis=1)[0:4])
@@ -122,7 +128,7 @@ class Test_static_info(unittest.TestCase):
             'ParentId':[-1, 19, 20, 20],
             'Lifetime':[-1, -1, -1, -1],
             'EnterTime':[0, 0, 0, 0],
-            'ExitTime':[-1.0, -1.0, -1.0, -1.0]
+            'ExitTime':[7.0, 7.0, 7.0, 7.0]
         })
         obs = oup.create_agents_table(self.output_file1)
         assert_frame_equal(exp,obs.drop('SimId', axis=1)[0:4])
