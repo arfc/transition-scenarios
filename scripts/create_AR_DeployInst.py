00c41bd3a3474714dc609e89de755c7cc90567d4
abachma2 <amandab7@illinois.edu>
2022-05-31 15:30:57 -0500

    create function determine_power_gap

--- scripts/create_AR_DeployInst.py
+++ scripts/create_AR_DeployInst.py
@@ -184,93 +184,36 @@ def get_deployed_power(power_dict, deployed_dict, sim_duration):
         based on the power and duration of each prototype
     '''
     t = np.arange(sim_duration)
-    inst_power = {}
     for key, val in deployed_dict.items():
-        inst_power = np.zeros(len(t))
+        power_profile = np.zeros(len(t))
         for i, v in enumerate(deployed_dict['prototype']):
             prototype_power = np.zeros(len(t))
             prototype_power[deployed_dict['build_times'][i] : deployed_dict['build_times'][i] + deployed_dict['lifetime'][i]] += power_dict[v] * deployed_dict['n_build'][i]
-            inst_power += prototype_power   
-    return t, inst_power
+            power_profile += prototype_power   
+    return t, power_profile
     
 
-def deploy_to_meet_demand(d, a, z, demand_eq, proto_list, ratio_list, lifetime_list, newinst_name):
-    filtered =  ''.join(x for x in demand_eq if x.isalpha())
-    if len(filtered) != 0 and 'x' not in filtered:
-        print(filtered)
-        raise ValueError('The variable is `x`')
-        
-    power_dict, deployed_dict, duration = get_config_dict(d)
-    t, inst_power = get_deployed_power(power_dict, deployed_dict, duration)
-    power = np.zeros(duration)
-    for key, val in inst_power.items():
-        power += val
-    # find lack
-    demand = np.zeros(len(t))
-    demand[a:z] = [eval(demand_eq) for x in t[a:z]]
-    #print('demand')
-    #print(demand[a:z])
-    lack = demand - power
-    lack[0] = 0
-    ratio_list = np.array(ratio_list)
-    ratio_list = ratio_list / sum(ratio_list)
-    lifetime_dict = {proto_list[i]: lifetime_list[i] for i, v in enumerate(lifetime_list)}
-    deploy_dict = {k:np.zeros(len(t)) for k in proto_list}
-    # split lack
-    lack_dict = {proto_list[i]: lack * ratio_list[i] for i, v in enumerate(ratio_list)}
-    for proto, l in lack_dict.items():
-        p = power_dict[proto]
-        life = lifetime_dict[proto]
-        for indx in range(duration):
-            while lack_dict[proto][indx] > 0:
-                deploy_dict[proto][indx] += 1
-                lack_dict[proto][indx: indx+life] -= p
-    
-    append_d = {'name': newinst_name,
-                'config': {'DeployInst': {'prototypes': {'val': []},
-                                          'n_build': {'val': []},
-                                          'build_times': {'val': []},
-                                          'lifetimes': {'val': []}}
-                            }
-               }
-    tot = 0
-    for key in deploy_dict:
-        for indx, val in enumerate(deploy_dict[key]):
-            if val == 0:
-                continue
-            tot += 1
-            append_d['config']['DeployInst']['prototypes']['val'].append(key)
-            append_d['config']['DeployInst']['n_build']['val'].append(int(val))
-            append_d['config']['DeployInst']['build_times']['val'].append(int(indx))
-            append_d['config']['DeployInst']['lifetimes']['val'].append(int(lifetime_dict[key]))
-   
-    if tot == 0:
-        return copy.deepcopy(d)
-        #for i in range(3):
-        #    append_d['config']['DeployInst']['prototypes']['val'].append('dummy')
-        #    append_d['config']['DeployInst']['n_build']['val'].append(1)
-        #    append_d['config']['DeployInst']['build_times']['val'].append(1)
-        #    append_d['config']['DeployInst']['lifetimes']['val'].append(1)
-    
+def determine_power_gap(power_profile, demand):
+    '''
+    Calculates the amount of power needed to be supplied 
+    based on the power produced and the demand equation
 
-    new_xml = copy.deepcopy(d)
-    new_xml['simulation']['region']['institution'].append(append_d)
-    return new_xml
+    Parameters:
+    ----------
+    power_profile: array
+        Amount of power produced at each time step
+    demand: array
+        evaluated values of the power demand equation used
 
-def stacked_bar(x, y):
-    fig = plt.figure()
-    a2 = fig.add_subplot(111)
-    prev = copy.deepcopy(np.zeros(len(x)))
-    for key, val in y.items():
-        a2.bar(x, height=val, width=1, bottom=prev, label=key+' power')
-        prev += val
-    a2.set_title('Power capacity')
-    a2.set_xlabel('Timesteps')
-    a2.set_ylabel('Power Capacity')
-    a2.grid()
-    a2.legend()
-    print('show')
-    plt.show()
+    Returns:
+    --------
+    power_gap: array
+        Amount of power needed to meet the power demand. Any negative 
+        values from an oversupply of power are changed to 0
+    '''
+    power_gap = demand - power_profile
+    power_gap[power_gap < 0] = 0
+    return power_gap 
     
 if __name__ == '__main__':
     simulation_input = "../input/haleu/inputs/united_states_2020.xml"
