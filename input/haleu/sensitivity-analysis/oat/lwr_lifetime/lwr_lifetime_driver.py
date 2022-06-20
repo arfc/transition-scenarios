2c7a37adc65c7e85c2cb9eb15072a354ff7ee6a5
abachma2 <amandab7@illinois.edu>
2022-06-20 15:57:13 -0500

    update file path to internal cyclus-files directory

--- input/haleu/sensitivity-analysis/oat/lwr_lifetime/lwr_lifetime_driver.py
+++ input/haleu/sensitivity-analysis/oat/lwr_lifetime/lwr_lifetime_driver.py
@@ -23,7 +23,7 @@ params, results = di.read_parameters_file()
 cyclus_template = 'lwr_lifetime_input.xml.in'
 scenario_name = 'lwr_' + str(round(params['lwr']))
 variable_dict = {'handle': scenario_name, 'lwr_lifetime':str(int(params['lwr']))}
-output_xml = '../../cyclus-files/scenario7.xml'
+output_xml = './cyclus-files/scenario7.xml'
 inp.render_input(cyclus_template, variable_dict, output_xml)
 
 # Create DeployInst for LWRs
@@ -35,23 +35,23 @@ DI_dict['DeployInst']['lifetimes']['val'][0] = 600
 length = len(DI_dict['DeployInst']['lifetimes']['val']) -1
 number_extended = int(np.round(length*int(params['lwr'])/100,0))
 DI_dict['DeployInst']['lifetimes']['val'][1:number_extended+1] = 80
-cdi.write_deployinst(DI_dict, 'lwr_' + str(int(params['lwr'])) + '_deployinst.xml')
+cdi.write_deployinst(DI_dict, './cyclus-files/lwr_' + str(int(params['lwr'])) + '_deployinst.xml')
 
 # Create DeployInst for advanced reactors 
 duration = 1500
 reactor_prototypes = {'Xe-100':(75, 720), 'MMR':(10,240), 'VOYGR':(50, 720)}
 demand_equation = np.zeros(duration)
 demand_equation[721:] = 89456.55
-deployinst = cdi.convert_xml_to_dict("lwr_"+str(int(params['lwr'])) + '_deployinst.xml')
+deployinst = cdi.convert_xml_to_dict("./cyclus-files/lwr_"+str(int(params['lwr'])) + '_deployinst.xml')
 lwr_powers = cdi.get_pris_powers('UNITED STATES OF AMERICA',"../../../../../database/", 2020)
 deployed_lwr_dict = cdi.get_deployinst_dict(deployinst, lwr_powers, "../../../inputs/united_states/reactors/")
 time, deployed_power = cdi.get_deployed_power(lwr_powers, deployed_lwr_dict, duration)
 power_gap = cdi.determine_power_gap(deployed_power, demand_equation)
 deploy_schedule = cdi.determine_deployment_schedule(power_gap, reactor_prototypes)
-cdi.write_deployinst(deploy_schedule, "AR_DeployInst_lwr_" + str(int(params['lwr'])) +".xml")
+cdi.write_deployinst(deploy_schedule, "./cyclus-files/AR_DeployInst_lwr_" + str(int(params['lwr'])) +".xml")
 
 # Run Cyclus with edited input file
-output_sqlite = '../../cyclus-files/' + scenario_name + '.sqlite'
+output_sqlite = './cyclus-files/' + scenario_name + '.sqlite'
 os.system('rm ' + output_sqlite)
 os.system('cyclus -i ' + output_xml + ' -o ' + output_sqlite + \
     ' --warn-limit 2')
