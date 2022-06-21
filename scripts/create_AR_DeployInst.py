a0e729708e5fc316702a2487357ebd364cbff5cf
abachma2 <amandab7@illinois.edu>
2022-06-21 15:46:49 -0500

    add slash for default path in get_deployed_dict

--- scripts/create_AR_DeployInst.py
+++ scripts/create_AR_DeployInst.py
@@ -25,7 +25,7 @@ def convert_xml_to_dict(filename):
     return xml_dict
 
 
-def get_deployinst_dict(deployinst_dict, power_dict, path="../input/haleu/inputs/united_states/reactors"):
+def get_deployinst_dict(deployinst_dict, power_dict, path="../input/haleu/inputs/united_states/reactors/"):
     '''
     Removes any non-power producing prototypes from the dictionary of
     the DeployInst. This also removes the 'val' level of information.
