2fc08e0b0e45e8389fb68150036ae4159a977446
abachma2 <amandab7@illinois.edu>
2022-07-07 10:40:43 -0500

    add lwr variable and create DI for LWRs

--- input/haleu/sensitivity-analysis/synergistic/ts_lwr/ts_lwr_driver.py
+++ input/haleu/sensitivity-analysis/synergistic/ts_lwr/ts_lwr_driver.py
@@ -20,13 +20,36 @@ params, results = di.read_parameters_file()
 # -------------------------------
 
 # Edit Cyclus input file
-cyclus_template = 'transition_start_input.xml.in'
-scenario_name = 'ty_' + str(round(params['ts']))
-variable_dict = {'handle': scenario_name, 'start_transition': int(params['ts'])}
-output_xml = './cyclus-files/transition_start_' + str(params['ts']) + '.xml'
+cyclus_template = 'ts_lwr_input.xml.in'
+scenario_name = 'ts_' + str(round(params['ts'])) + '_lwr_' + \
+    str(round(params['lwr']))
+variable_dict = {'handle': scenario_name, 
+                 'start_transition': int(params['ts']), 
+                 'lwr':int(params['lwr'])}
+output_xml = './cyclus-files/ts_' + str(params['ts']) + '_lwr_' + \
+    str(params['lwr']) + '.xml'
 inp.render_input(cyclus_template, variable_dict, output_xml)
 
-# Create DeployInst 
+# Create DeployInst for LWRs
+DI_dict = cdi.convert_xml_to_dict("initial_lwr_deployinst.xml")
+DI_dict['DeployInst']['lifetimes'] = {'val':[]}
+DI_dict['DeployInst']['lifetimes']['val'] = np.repeat(720, 116)
+DI_dict['DeployInst']['lifetimes']['val'][0] = 600
+
+with open("../../../../../database/lwr_power_order.txt", 'r') as f:
+    lwrs = f.readlines()
+for index, item in enumerate(lwrs):
+    lwrs[index] = item.strip("\n")
+lwrs_extended = lwrs[:int(params['lwr'])]
+
+for lwr in lwrs_extended:
+    index = DI_dict['DeployInst']['prototypes']['val'].index(lwr)
+    DI_dict['DeployInst']['lifetimes']['val'][index] = 960
+cdi.write_deployinst(DI_dict, './cyclus-files/ts_' + \
+    str(params['ts']) + 'lwr_' + str(int(params['lwr'])) + \
+    '_deployinst.xml')
+
+# Create DeployInst for advanced reactors
 duration = 1500
 reactor_prototypes = {'Xe-100':(80, 720), 'MMR':(5,240), 'VOYGR':(77, 720)}
 demand_equation = np.zeros(duration)
@@ -37,7 +60,9 @@ deployed_lwr_dict = cdi.get_deployinst_dict(deployinst, lwr_powers, "../../../in
 time, deployed_power = cdi.get_deployed_power(lwr_powers, deployed_lwr_dict, duration)
 power_gap = cdi.determine_power_gap(deployed_power, demand_equation)
 deploy_schedule = cdi.determine_deployment_schedule(power_gap, reactor_prototypes)
-cdi.write_deployinst(deploy_schedule, "./cyclus-files/ty_" + str(int(params['ts'])) +"_deployinst.xml")
+cdi.write_deployinst(deploy_schedule, "./cyclus-files/ts_" + \
+    str(int(params['ts'])) + '_lwr_' + str(params['lwr']) + \
+    "_deployinst.xml")
 
 # Run Cyclus with edited input file
 output_sqlite = './cyclus-files/' + scenario_name + '.sqlite'
