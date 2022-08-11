c19c01154893ae7ae6ab881c78baaf5e6b7ebc03
abachma2 <amandab7@illinois.edu>
2022-08-11 08:36:44 -0500

    add CFs, change DI file name

--- input/haleu/sensitivity-analysis/synergistic/ts_lwr/ts_lwr_driver.py
+++ input/haleu/sensitivity-analysis/synergistic/ts_lwr/ts_lwr_driver.py
@@ -31,7 +31,7 @@ output_xml = './cyclus-files/ts_' + str(int(params['ts'])) + '_lwr_' + \
 inp.render_input(cyclus_template, variable_dict, output_xml)
 
 # Create DeployInst for LWRs
-DI_dict = cdi.convert_xml_to_dict("../../initial_lwr_deployinst.xml")
+DI_dict = cdi.convert_xml_to_dict("../../../inputs/united_states/buildtimes/UNITED_STATES_OF_AMERICA/deployinst.xml")
 DI_dict['DeployInst']['lifetimes'] = {'val':[]}
 DI_dict['DeployInst']['lifetimes']['val'] = np.repeat(720, 116)
 DI_dict['DeployInst']['lifetimes']['val'][0] = 600
@@ -51,14 +51,15 @@ cdi.write_deployinst(DI_dict, './cyclus-files/ts_' + \
 
 # Create DeployInst for advanced reactors
 duration = 1500
-reactor_prototypes = {'Xe-100':(80, 720), 'MMR':(5,240), 'VOYGR':(77, 720)}
+reactor_prototypes = {'Xe-100':(76, 720), 'MMR':(5,240), 'VOYGR':(73, 720)}
 demand_equation = np.zeros(duration)
-demand_equation[int(params['ts']):] = 89456.55
-deployinst = cdi.convert_xml_to_dict("../../../inputs/united_states/buildtimes/UNITED_STATES_OF_AMERICA/deployinst.xml")
+demand_equation[int(params['ts']):] = 87198.156
+deployinst = cdi.convert_xml_to_dict("./cyclus-files/ts_" + str(int(params['ts'])) +\
+				     "_lwr_" + str(int(params['lwr'])) + "_deployinst.xml")
 lwr_powers = cdi.get_pris_powers('UNITED STATES OF AMERICA',"../../../../../database/", 2020)
 deployed_lwr_dict = cdi.get_deployinst_dict(deployinst, lwr_powers, "../../../inputs/united_states/reactors/")
 time, deployed_power = cdi.get_deployed_power(lwr_powers, deployed_lwr_dict, duration)
-power_gap = cdi.determine_power_gap(deployed_power, demand_equation)
+power_gap = cdi.determine_power_gap(deployed_power*0.9266, demand_equation)
 deploy_schedule = cdi.determine_deployment_schedule(power_gap, reactor_prototypes)
 cdi.write_deployinst(deploy_schedule, "./cyclus-files/AR_ts_" + \
     str(int(params['ts'])) + '_lwr_' + str(int(params['lwr'])) + \
