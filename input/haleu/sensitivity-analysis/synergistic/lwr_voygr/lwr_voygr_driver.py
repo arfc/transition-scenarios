0cd71f68d37d22d400cf0d5be24fdd624d3dfcd2
abachma2 <amandab7@illinois.edu>
2022-08-11 08:13:50 -0500

    fix file paths and remove extra +s

--- input/haleu/sensitivity-analysis/synergistic/lwr_voygr/lwr_voygr_driver.py
+++ input/haleu/sensitivity-analysis/synergistic/lwr_voygr/lwr_voygr_driver.py
@@ -27,7 +27,7 @@ variable_dict = {'handle': scenario_name,
                  'lwr':str(int(params['lwr'])),
                  'voygr':str(int(params['voygr']))}
 output_xml = './cyclus-files/lwr_' + str(params['lwr']) +\
-             + '_voygr_' + str(int(params['voygr'])) + '.xml'
+             '_voygr_' + str(int(params['voygr'])) + '.xml'
 inp.render_input(cyclus_template, variable_dict, output_xml)
 
 # Create DeployInst for LWRs
@@ -45,7 +45,8 @@ lwrs_extended = lwrs[:int(params['lwr'])]
 for lwr in lwrs_extended:
     index = DI_dict['DeployInst']['prototypes']['val'].index(lwr)
     DI_dict['DeployInst']['lifetimes']['val'][index] = 960
-cdi.write_deployinst(DI_dict, './cyclus-files/lwr_' + str(int(params['lwr'])) + '_deployinst.xml')
+cdi.write_deployinst(DI_dict, './cyclus-files/lwr_' + str(int(params['lwr'])) + '_voygr_' +\
+			      str(int(params['voygr'])) + '_deployinst.xml')
 
 # Create DeployInst for advanced reactors 
 duration = 1500
@@ -53,14 +54,14 @@ reactor_prototypes = {'Xe-100':(76, 720), 'MMR':(5,240), 'VOYGR':(73, 720)}
 demand_equation = np.zeros(duration)
 demand_equation[721:] = 87198.156
 deployinst = cdi.convert_xml_to_dict("./cyclus-files/lwr_"+str(int(params['lwr'])) +\
-        '_voygr' + str(int(params['voygr'])) +'_deployinst.xml')
+        '_voygr_' + str(int(params['voygr'])) +'_deployinst.xml')
 lwr_powers = cdi.get_pris_powers('UNITED STATES OF AMERICA',"../../../../../database/", 2020)
 deployed_lwr_dict = cdi.get_deployinst_dict(deployinst, lwr_powers, "../../../inputs/united_states/reactors/")
 time, deployed_power = cdi.get_deployed_power(lwr_powers, deployed_lwr_dict, duration)
 power_gap = cdi.determine_power_gap(deployed_power*0.9266, demand_equation)
 deploy_schedule = cdi.determine_deployment_schedule(power_gap, reactor_prototypes, 'VOYGR', int(params['voygr']))
 cdi.write_deployinst(deploy_schedule, "./cyclus-files/AR_DeployInst_lwr_" + str(int(params['lwr'])) + \
-                                      + "_voygr_" + str(int(params['voygr'])) + ".xml")
+                                      "_voygr_" + str(int(params['voygr'])) + ".xml")
 
 # Run Cyclus with edited input file
 output_sqlite = './cyclus-files/' + scenario_name + '.sqlite'
