de6566138111c8fb0d0e2b789b90dd17240dfade
abachma2 <amandab7@illinois.edu>
2022-08-11 08:11:57 -0500

    update for CFs, demand, and small fixes

--- input/haleu/sensitivity-analysis/synergistic/lwr_voygr/lwr_voygr_driver.py
+++ input/haleu/sensitivity-analysis/synergistic/lwr_voygr/lwr_voygr_driver.py
@@ -31,7 +31,7 @@ output_xml = './cyclus-files/lwr_' + str(params['lwr']) +\
 inp.render_input(cyclus_template, variable_dict, output_xml)
 
 # Create DeployInst for LWRs
-DI_dict = cdi.convert_xml_to_dict("initial_lwr_deployinst.xml")
+DI_dict = cdi.convert_xml_to_dict("../../../inputs/united_states/buildtimes/UNITED_STATES_OF_AMERICA/deployinst.xml")
 DI_dict['DeployInst']['lifetimes'] = {'val':[]}
 DI_dict['DeployInst']['lifetimes']['val'] = np.repeat(720, 116)
 DI_dict['DeployInst']['lifetimes']['val'][0] = 600
@@ -49,18 +49,18 @@ cdi.write_deployinst(DI_dict, './cyclus-files/lwr_' + str(int(params['lwr'])) + 
 
 # Create DeployInst for advanced reactors 
 duration = 1500
-reactor_prototypes = {'Xe-100':(80, 720), 'MMR':(5,240), 'VOYGR':(77, 720)}
+reactor_prototypes = {'Xe-100':(76, 720), 'MMR':(5,240), 'VOYGR':(73, 720)}
 demand_equation = np.zeros(duration)
-demand_equation[721:] = 89456.55
+demand_equation[721:] = 87198.156
 deployinst = cdi.convert_xml_to_dict("./cyclus-files/lwr_"+str(int(params['lwr'])) +\
         '_voygr' + str(int(params['voygr'])) +'_deployinst.xml')
 lwr_powers = cdi.get_pris_powers('UNITED STATES OF AMERICA',"../../../../../database/", 2020)
 deployed_lwr_dict = cdi.get_deployinst_dict(deployinst, lwr_powers, "../../../inputs/united_states/reactors/")
 time, deployed_power = cdi.get_deployed_power(lwr_powers, deployed_lwr_dict, duration)
-power_gap = cdi.determine_power_gap(deployed_power, demand_equation)
+power_gap = cdi.determine_power_gap(deployed_power*0.9266, demand_equation)
 deploy_schedule = cdi.determine_deployment_schedule(power_gap, reactor_prototypes, 'VOYGR', int(params['voygr']))
 cdi.write_deployinst(deploy_schedule, "./cyclus-files/AR_DeployInst_lwr_" + str(int(params['lwr'])) + \
-                                      + "_voygr" + str(int(params['voygr'])) + ".xml")
+                                      + "_voygr_" + str(int(params['voygr'])) + ".xml")
 
 # Run Cyclus with edited input file
 output_sqlite = './cyclus-files/' + scenario_name + '.sqlite'
