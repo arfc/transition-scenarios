fe1514f0f9577c891f86ea572d8700a773c87324
abachma2 <amandab7@illinois.edu>
2022-09-30 09:25:33 -0500

    update drive files with new functions

--- input/haleu/sensitivity-analysis/synergistic/ts_mmr/ts_mmr_driver.py
+++ input/haleu/sensitivity-analysis/synergistic/ts_mmr/ts_mmr_driver.py
@@ -27,6 +27,7 @@ variable_dict = {'handle': scenario_name,
                  'mmr': int(params['mmr'])}
 output_xml = './cyclus-files/ts_' + str(int(params['ts'])) + \
     '_mmr_' + str(int(params['mmr'])) + '.xml'
+output_sqlite = './cyclus-files/' + scenario_name + '.sqlite'
 inp.render_input(cyclus_template, variable_dict, output_xml)
 
 # Create DeployInst
@@ -34,51 +35,25 @@ duration = 1500
 reactor_prototypes = {'Xe-100': (76, 720), 'MMR': (5, 240), 'VOYGR': (73, 720)}
 demand_equation = np.zeros(duration)
 demand_equation[int(params['ts']):] = 87198.156
-deployinst = cdi.convert_xml_to_dict(
-    "../../../inputs/united_states/buildtimes/UNITED_STATES_OF_AMERICA/deployinst.xml")
-lwr_powers = cdi.get_pris_powers(
-    'UNITED STATES OF AMERICA',
-    "../../../../../database/",
-    2020)
-deployed_lwr_dict = cdi.get_deployinst_dict(
-    deployinst, lwr_powers, "../../../inputs/united_states/reactors/")
-time, deployed_power = cdi.get_deployed_power(
-    lwr_powers, deployed_lwr_dict, duration)
-power_gap = cdi.determine_power_gap(deployed_power * 0.9266, demand_equation)
-deploy_schedule = cdi.determine_deployment_schedule(
-    power_gap, reactor_prototypes, 'MMR', int(params['mmr']))
+lwr_DI = cdi.convert_xml_to_dict("../../../inputs/united_states/" +
+                                 "buildtimes/UNITED_STATES_OF_AMERICA/" +
+                                 "deployinst.xml")
+deploy_schedule = cdi.write_AR_deployinst(
+    lwr_DI,
+    "../../../inputs/united_states/reactors/",
+    duration,
+    reactor_prototypes,
+    demand_equation,
+    'MMR',
+    int(params['mmr']))
 cdi.write_deployinst(deploy_schedule, "./cyclus-files/ts_" +
                      str(int(params['ts'])) +
                      '_mmr_' + str(int(params['mmr'])) + "_deployinst.xml")
 
 # Run Cyclus with edited input file
-output_sqlite = './cyclus-files/' + scenario_name + '.sqlite'
-os.system('rm ' + output_sqlite)
-os.system('cyclus -i ' + output_xml + ' -o ' + output_sqlite +
-          ' --warn-limit 2')
+oup.run_cyclus(output_sqlite, output_xml)
 
 # ----------------------------
 # Return the results to Dakota
 # ----------------------------
-results['enr_u'].function = oup.get_enriched_u_mass(output_sqlite,
-                                                    ['Xe-100', 'MMR', 'VOYGR'],
-                                                    params['ts'])
-results['haleu'].function = oup.get_enriched_u_mass(output_sqlite,
-                                                    ['Xe-100', 'MMR'],
-                                                    params['ts'])
-results['swu'].function = oup.calculate_swu(
-    output_sqlite, ['Xe-100', 'MMR', 'VOYGR'], params['ts'])
-results['haleu_swu'].function = oup.calculate_swu(
-    output_sqlite, ['Xe-100', 'MMR'], params['ts'])
-results['waste'].function = oup.get_waste_discharged(output_sqlite,
-                                                     ['Xe-100', 'MMR', 'VOYGR'],
-                                                     params['ts'],
-                                                     {'MMR': 'spent_MMR_haleu',
-                                                      'Xe-100': 'spent_xe100_haleu',
-                                                      'VOYGR': 'spent_smr_fuel'}
-                                                     )
-results['feed'].function = oup.calculate_feed(output_sqlite,
-                                              ['Xe-100', 'MMR'],
-                                              params['ts'])
-
-results.write()
+results = oup.get_all_results(results, output_sqlite)
