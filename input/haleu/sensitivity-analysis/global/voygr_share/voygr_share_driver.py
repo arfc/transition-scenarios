5afcafa496efaaac4f2926bafb0757ef1f748b91
abachma2 <amandab7@illinois.edu>
2022-11-28 10:32:46 -0600

    update files for global analysis
    Update the variable names, update function input parameters, and
    add variations in the MMR and Xe-100 burnups. Still need to
    account for list of parameters, see if that's compatible with
    the LHS method

--- input/haleu/sensitivity-analysis/global/voygr_share/voygr_share_driver.py
+++ input/haleu/sensitivity-analysis/global/voygr_share/voygr_share_driver.py
@@ -19,17 +19,17 @@ params, results = di.read_parameters_file()
 # -------------------------------
 
 # Edit Cyclus input file
-cyclus_template = 'ts_lwr_voygr_input.xml.in'
-scenario_name = 'ts_' + str(int(params['ts'])) +\
-                '_lwr_' + str(int(params['lwr'])) + '_voygr_' + \
-                str(int(params['voygr']))
+cyclus_template = 'voygr_share_input.xml.in'
+scenario_name = 'voygr_share_' + \
+                str(int(params['voygr_share']))
 variable_dict = {'handle': scenario_name,
                  'ts': str(int(params['ts'])),
                  'lwr': str(int(params['lwr'])),
-                 'voygr': str(int(params['voygr']))}
-output_xml = './cyclus-files/ts_' + str(int(params['ts'])) +\
-             '_lwr_' + str(int(params['lwr'])) +\
-             '_voygr_' + str(int(params['voygr'])) + '.xml'
+                 'voygr_share': str(int(params['voygr_share'])),
+                 'mmr_burnup':str(int(params['mmr_burnup'])),
+                 'xe100_burnup':str(int(params['xe100_burnup']))}
+output_xml = './cyclus-files/voygr_share_' + 
+             str(int(params['voygr_share'])) + '.xml'
 output_sqlite = './cyclus-files/' + scenario_name + '.sqlite'
 inp.render_input(cyclus_template, variable_dict, output_xml)
 
@@ -39,32 +39,30 @@ DI_dict = cdi.write_lwr_deployinst(
     "../../../inputs/united_states/buildtimes/" +
     "UNITED_STATES_OF_AMERICA/deployinst.xml",
     "../../../../../database/lwr_power_order.txt")
-cdi.write_deployinst(DI_dict, './cyclus-files/ts_' + str(int(params['ts'])) +\
-                              '_lwr_' + str(int(params['lwr'])) +
-                              '_voygr_' + str(int(params['voygr'])) +
+cdi.write_deployinst(DI_dict, './cyclus-files/' + 
+                              scenario_name +
                               '_deployinst.xml')
 
 # Create DeployInst for advanced reactors
 duration = 1500
-reactor_prototypes = {'Xe-100': (76, 720), 'MMR': (5, 240), 'VOYGR': (73, 720)}
+mmr_lifetimes = {41:120, 62:180, 74:218, 78:231, 82:240, 86:255, 90:267}
+reactor_prototypes = {'Xe-100': (76, 720), 
+                      'MMR': (5, mmr_lifetimes[int(params['mmr_burnup'])]), 
+                      'VOYGR': (73, 720)}
 demand_equation = np.zeros(duration)
 demand_equation[int(params['ts']):] = 87198.156
-lwr_DI = cdi.convert_xml_to_dict('./cyclus-files/ts_' + str(int(params['ts'])) +\
-                              '_lwr_' + str(int(params['lwr'])) +
-                              '_voygr_' + str(int(params['voygr'])) +
-                              '_deployinst.xml')
+lwr_DI = cdi.convert_xml_to_dict('./cyclus-files/' +
+                                 scneario_name +
+                                 '_deployinst.xml')
 deploy_schedule = cdi.write_AR_deployinst(
     lwr_DI,
     "../../../inputs/united_states/reactors/",
     duration,
     reactor_prototypes,
     demand_equation,
-    'VOYGR',
-    int(params['voygr']))
-cdi.write_deployinst(deploy_schedule, "./cyclus-files/AR_DeployInst_ts_" +
-                                      str(int(params['ts'])) + "_lwr_" +
-                                      str(int(params['lwr'])) +
-                                      "_voygr_" + str(int(params['voygr'])) +
+    {'VOYGR':int(params['voygr_share'])})
+cdi.write_deployinst(deploy_schedule, "./cyclus-files/AR_DeployInst_" +
+                                      scenario_name + 
                                       ".xml")
 
 # Run Cyclus with edited input file
