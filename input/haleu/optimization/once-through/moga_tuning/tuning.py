36ce90473fa8a605ce802e639331a960ceb90b69
abachma2 <amandab7@illinois.edu>
2023-04-04 10:01:53 -0500

    change pop size of 10 to 100

--- input/haleu/optimization/once-through/moga_tuning/tuning.py
+++ input/haleu/optimization/once-through/moga_tuning/tuning.py
@@ -7,13 +7,29 @@ import dakota_input as inp
 
 counter = 0
 for c_rate in [0.3, 0.584, 0.8]:
-    for pop_size in [10, 25, 50]:
-        for m_rate in [0.08, 0.1, 0.116, 0.15]:
-            variable_dict = {'pop_size':pop_size,
-                            'mutation_rate':m_rate,
-                            'crossover_rate':c_rate,
-                            'counter':counter}
-            dakota_file = f"tuning_{counter}.in"
-            inp.render_input("moga_tuning_template.in", variable_dict, dakota_file)
+    variable_dict = {'pop_size':50,
+                     'mutation_rate':0.08,
+                     'crossover_rate':c_rate,
+                     'counter':counter}
+    dakota_file = f"tuning_{counter}.in"
+    inp.render_input("moga_tuning_template.in", variable_dict, dakota_file)
 
-            counter += 1
+    counter += 1
+for pop_size in [100, 25, 50]:
+    variable_dict = {'pop_size':pop_size,
+                     'mutation_rate':0.08,
+                     'crossover_rate':0.8,
+                     'counter':counter}
+    dakota_file = f"tuning_{counter}.in"
+    inp.render_input("moga_tuning_template.in", variable_dict, dakota_file)
+    counter += 1
+
+for m_rate in [0.08, 0.1, 0.116, 0.15]:
+    variable_dict = {'pop_size':25,
+                     'mutation_rate':m_rate,
+                     'crossover_rate':0.8,
+                     'counter':counter}
+    dakota_file = f"tuning_{counter}.in"
+    inp.render_input("moga_tuning_template.in", variable_dict, dakota_file)
+
+    counter += 1
