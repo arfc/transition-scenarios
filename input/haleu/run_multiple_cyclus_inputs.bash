92533d680661e4cdd4441af2b30735b853e66031
Amanda <amandab7@illinois.edu>
2023-10-09 13:42:15 -0500

    update to reflect input file name changes

--- input/haleu/run_multiple_cyclus_inputs.bash
+++ input/haleu/run_multiple_cyclus_inputs.bash
@@ -1,8 +1,5 @@
-for scenario in mmr xe100 xe100_mmr mmr_voygr xe100_voygr xe100_mmr_voygr 
+for scenario in 2 3 4 5 6 7 8 9 10 11 12 13
 do 
-  for growth in nogrowth 1percent
-  do
-    rm "outputs/${scenario}_${growth}.sqlite"
-    cyclus -i "inputs/${scenario}_${growth}.xml" -o "outputs/${scenario}_${growth}.sqlite"
-  done
+  rm "outputs/scenario${scenario}.sqlite"
+    cyclus -i "inputs/scenario${scenario}.xml" -o "outputs/scenario${scenario}.sqlite"
 done
