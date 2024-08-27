145282cb76378bfae4a1cca95e3484b846b78728
Nathan Ryan <nsryan2@illinois.edu>
2024-08-27 14:12:18 -0500

    add deployment function tests

--- scripts/tests/test_deployment_script.py
+++ scripts/tests/test_deployment_script.py
@@ -48,3 +48,120 @@ def test_num_react_to_cap():
     assert all(react_to_cap_df['manual_cap'][i] == react_to_cap_df['new_cap'][i] for i in range(len(react_to_cap_df['manual_cap'])))
 
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # Deployment Functions # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+def test_greedy_deployment():
+    # Greedy distribution dictionary
+    greedy_dist_df = {
+        'Year': [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024],
+        'test_cap': [20, 79, 80, 81, 220, 640, 693, 950, 700],
+        'manual_cap': [20, 75, 80, 80, 220, 640, 690, 950, 700]}
+
+    # Convert to DataFrame for comparison
+    greedy_dist_df = pd.DataFrame(greedy_dist_df)
+
+    calculated_greedy_df = dep.greedy_deployment(
+        test_df, 'test_cap', ad_reactors)
+
+    # Ensure 'new_cap' column exists in the result
+    assert 'new_cap' in calculated_greedy_df.columns, "The 'new_cap' column is missing in the calculated results."
+
+    # Test the 'manual_cap' values against 'new_cap'
+    for i in range(len(greedy_dist_df)):
+        assert greedy_dist_df['manual_cap'][i] == calculated_greedy_df['new_cap'][i], f"Failed at index {i}: {greedy_dist_df['manual_cap'][i]} != {calculated_greedy_df['new_cap'][i]}"
+
+    print("Greedy tests passed.")
+
+
+def test_pre_det_deployment_greedy():
+    # Define the expected DataFrame for the greedy case
+    manual_pre_det_greedy_df = {
+        'Year': [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024],
+        'test_cap': [20, 79, 80, 81, 220, 640, 693, 950, 700],
+        'total_cap': [20, 75, 80, 95, 240, 715, 690, 965, 950],
+        'manual_cap': [20, 75, 80, 80, 220, 640, 690, 950, 700]
+    }
+    manual_pre_det_greedy_df = pd.DataFrame(manual_pre_det_greedy_df)
+
+    # Create a copy of test_df to pass to the function
+    pre_det_dep_df_greedy = test_df.copy()
+
+    # Call the pre_det_deployment function with greedy=True
+    result_df = dep.pre_det_deployment(pre_det_dep_df_greedy, 'test_cap', ad_reactors, greedy=True)
+
+    # Check that 'new_cap' matches 'manual_cap'
+    assert result_df['new_cap'].equals(manual_pre_det_greedy_df['manual_cap'])
+
+    print("Greedy pre-determined distribution test passed.")
+
+
+def test_pre_det_deployment_linear():
+    # Define the expected DataFrame for the linear case
+    pre_det_linear_df = {
+        'Year': [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024],
+        'test_cap': [20, 79, 80, 81, 220, 640, 693, 950, 700],
+        'total_cap': [80, 80, 100, 100, 225, 655, 825, 1150, 1050],
+        'manual_cap': [80, 80, 100, 100, 225, 655, 700, 955, 705]
+    }
+    pre_det_linear_df = pd.DataFrame(pre_det_linear_df)
+
+    # Create a copy of test_df to pass to the function
+    pre_det_dep_df_linear = test_df.copy()
+
+    # Call the pre_det_deployment function with greedy=False
+    result_df = dep.pre_det_deployment(pre_det_dep_df_linear, 'test_cap', ad_reactors, greedy=False)
+
+    # Check that 'new_cap' matches 'manual_cap'
+    assert result_df['new_cap'].equals(pre_det_linear_df['manual_cap'])
+
+    print("Linear pre-determined distribution test passed.")
+
+
+def test_rand_deployment():
+    # Define the expected DataFrame for the random function
+    manual_rand_df = {
+        'Year': [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024],
+        'test_cap': [20, 79, 80, 81, 220, 640, 693, 950, 700],
+        'total_cap': [0, 0, 80, 80, 205, 615, 620, 915, 835],
+        'manual_cap': [0, 0, 80, 80, 205, 615, 615, 900, 695]
+    }
+
+    manual_rand_df = pd.DataFrame(manual_rand_df)
+
+    # Create a copy of test_df to pass to the function
+    rand_dep_df = test_df.copy()
+
+    # Call the rand_deployment function
+    result_df = dep.rand_deployment(rand_dep_df, 'test_cap', ad_reactors, set_seed=True)
+
+    # Check that 'new_cap' matches 'manual_cap'
+    assert result_df['new_cap'].equals(manual_rand_df['manual_cap'])
+
+    print("Random deployment test passed.")
+
+
+def test_rand_greedy_deployment():
+    # Define the expected DataFrame for the random greedy function
+    manual_rand_greedy_df = {
+        'Year': [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024],
+        'test_cap': [20, 79, 80, 81, 220, 640, 693, 950, 700],
+        'total_cap': [20, 75, 80, 95, 240, 715, 690, 965, 950],
+        'manual_cap': [20, 75, 80, 80, 220, 640, 690, 950, 700]
+    }
+
+    manual_rand_greedy_df = pd.DataFrame(manual_rand_greedy_df)
+
+    # Create a copy of test_df to pass to the function
+    rand_greedy_dep_df = test_df.copy()
+
+    # Call the rand_deployment function
+    result_df = dep.rand_greedy_deployment(rand_greedy_dep_df, 'test_cap', ad_reactors, set_seed=True)
+
+    # Check that 'new_cap' matches 'manual_cap'
+    assert result_df['new_cap'].equals(manual_rand_greedy_df['manual_cap'])
+
+    print("Random greedy deployment test passed.")
+
+
