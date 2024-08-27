78a13b86283f9742c930260af4aec8954142c2e2
Nathan Ryan <nsryan2@illinois.edu>
2024-08-27 14:08:49 -0500

    add pre determined deployment

--- scripts/deployment_script.py
+++ scripts/deployment_script.py
@@ -134,3 +134,102 @@ def greedy_deployment(df, base_col, ar_dict):
     return df
 
 
+def pre_det_deployment(df, base_col, ar_dict, greedy=True):
+    """
+    This function allows the user to specify a distribution for reactor
+    deployment for specific models.
+
+    There are two implementations:
+    1) greedy (greedy=True): wherein the largest reactor is deployed up till
+    its cap, then the next largest and so on.
+    2) linear (greedy=False): wherein the capped reactors are cyclically
+    deployed until they hit their individual cap.
+
+    df: pandas dataframe
+        The dataframe of capacity information.
+    base_col: str
+        The string name corresponding to the column of capacity that the
+        algorithm is deploying reactors to meet.
+    ar_dict: dictionary
+        A dictionary of reactors with information of the form:
+        {reactor: [Power (MWe), capacity_factor (%), lifetime (yr),
+        [distribution]]}.
+        * The distribution must be the same length as the capacity you are
+        matching.
+        * The function assumes that the distribution is at the third index
+        (i.e., fourth spot).
+    greedy: bool
+        A True/False value that determines whether the initial deployment is
+        greedy or linear.
+    """
+    # initialize the number of reactor columns
+    for reactor in ar_dict.keys():
+        if f'num_{reactor}' not in df:
+            df[f'num_{reactor}'] = 0
+        else:
+            pass
+
+    if greedy == True:
+        for year in range(len(df[base_col])):
+            cap_difference = df[base_col][year].copy()
+            for reactor in ar_dict.keys():
+                # The number of reactors you'd need to meet the remaining
+                # capacity.
+                most_reactors = math.floor(cap_difference/ar_dict[reactor][0])
+                # If there is a distribution we'll use it. Otherwise, we
+                # will use a greedy deployment.
+                if type(ar_dict[reactor][3]) == list:
+                    # Check if the desired amount is greater than the cap.
+                    if most_reactors >= ar_dict[reactor][3][year]:
+                        # If so, we will only deploy up to the cap.
+                        cap_difference -= ar_dict[reactor][3][year] * ar_dict[reactor][0]
+                        df.loc[year, f'num_{reactor}'] += ar_dict[reactor][3][year]
+                    # If the desired is less than the cap, then we'll deploy a
+                    # greedy amount of reactors.
+                    else:
+                        cap_difference -= most_reactors * ar_dict[reactor][0]
+                        df.loc[year, f'num_{reactor}'] += most_reactors
+                # If there's no cap then we'll deploy a greedy amount of
+                # reactors.
+                else:
+                    cap_difference -= most_reactors * ar_dict[reactor][0]
+                    df.loc[year, f'num_{reactor}'] += most_reactors
+    # Not greedy.
+    else:
+        for year in range(len(df[base_col])):
+            cap_difference = df[base_col][year].copy()
+            while cap_difference > 0:
+                for reactor in ar_dict.keys():
+                    # Check if the next reactor will exceed or equal the
+                    # desired capacity.
+                    next_difference = cap_difference - ar_dict[reactor][0]
+                    # Set the limit one of the smallest reactors below zero
+                    # so the while loop can end.
+                    if next_difference <= -(ar_dict[reactor][0] + 1):
+                        continue
+                    else:
+                        # Check if a cap exists.
+                        if type(ar_dict[reactor][3]) == list:
+                            # Check if the cap will be exceeded by adding
+                            # another reactor.
+                            if ar_dict[reactor][3][year] >= (df.loc[year,f'num_{reactor}'] + 1):
+                                df.loc[year, f'num_{reactor}'] += 1
+                                # Update remaining capacity to be met.
+                                cap_difference -= ar_dict[reactor][0]
+                        # If there isn't a cap add another reactor.
+                        else:
+                            df.loc[year, f'num_{reactor}'] += 1
+                            # Update remaining capacity to be met.
+                            cap_difference -= ar_dict[reactor][0]
+
+    # account for decommissioning with a direct replacement
+    df = direct_decom(df, ar_dict)
+
+    # Now calculate the total capacity each year (includes capacity from a
+    # replacement reactor that is new that year, but not new overall because it
+    # is replacing itself).
+    df  = num_react_to_cap(df, ar_dict)
+
+    return df
+
+
